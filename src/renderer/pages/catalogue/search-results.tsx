79e08889f22c187690823240d7a3ab10bec95b84
Guilherme Viana <imluizguilara@hotmail.com>
2024-04-14 09:41:30 -0300

    feat/no-results-ui: add items not found ui on catalogue search

--- src/renderer/pages/catalogue/search-results.tsx
+++ src/renderer/pages/catalogue/search-results.tsx
@@ -1,13 +1,14 @@
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { GameCard } from "@renderer/components";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 import type { CatalogueEntry } from "@types";
 
-import * as styles from "./catalogue.css";
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "@renderer/hooks";
+import { InboxIcon } from "@primer/octicons-react";
 import { clearSearch } from "@renderer/features";
+import { useAppDispatch, useAppSelector } from "@renderer/hooks";
 import { vars } from "@renderer/theme.css";
+import { useNavigate } from "react-router-dom";
+import * as styles from "./catalogue.css";
 
 export function SearchResults() {
   const { results, isLoading } = useAppSelector((state) => state.search);
@@ -22,22 +23,38 @@ export function SearchResults() {
 
   return (
     <SkeletonTheme baseColor={vars.color.background} highlightColor="#444">
-      <section className={styles.content}>
-        <section className={styles.cards({ searching: false })}>
-          {isLoading
-            ? Array.from({ length: 12 }).map((_, index) => (
-                <Skeleton key={index} className={styles.cardSkeleton} />
-              ))
-            : results.map((game) => (
-                <GameCard
-                  key={game.objectID}
-                  game={game}
-                  onClick={() => handleGameClick(game)}
-                  disabled={!game.repacks.length}
-                />
-              ))}
-        </section>
-      </section>
+      <main className={styles.content}>
+				<section className={styles.cards({ searching: false })}>
+					{isLoading &&
+						Array.from({ length: 12 }).map((_, index) => (
+							<Skeleton
+								key={index}
+								className={styles.cardSkeleton}
+							/>
+						))}
+
+					{!isLoading && results.length > 0 && (
+						<>
+							{results.map((game) => (
+								<GameCard
+									key={game.objectID}
+									game={game}
+									onClick={() => handleGameClick(game)}
+									disabled={!game.repacks.length}
+								/>
+							))}
+						</>
+					)}
+				</section>
+
+				{!isLoading && results.length === 0 && (
+					<div className={styles.noResults}>
+						<InboxIcon size={56} />
+
+						<p>Nenhum resultado encontrado</p>
+					</div>
+				)}
+			</main>
     </SkeletonTheme>
   );
 }
