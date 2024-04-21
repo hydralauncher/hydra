149c07c89d4764c66fed24690466124ae935b995
José Luís <peressousa77@gmail.com>
2024-04-21 01:02:17 -0300

    prettier: format all files

--- src/main/events/hardware/get-disk-free-space.ts
+++ src/main/events/hardware/get-disk-free-space.ts
@@ -2,8 +2,10 @@ import checkDiskSpace from "check-disk-space";
 
 import { registerEvent } from "../register-event";
 
-const getDiskFreeSpace = async (_event: Electron.IpcMainInvokeEvent, path: string) =>
-  checkDiskSpace(path);
+const getDiskFreeSpace = async (
+  _event: Electron.IpcMainInvokeEvent,
+  path: string
+) => checkDiskSpace(path);
 
 registerEvent(getDiskFreeSpace, {
   name: "getDiskFreeSpace",
