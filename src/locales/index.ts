8f4b0cf2dfa3d187c74c3d64bc112cb322303a95
FeriVOQ <36544651+ferivoq@users.noreply.github.com>
2024-04-21 21:11:37 +0200

    Added hu to index.ts

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,3 +2,4 @@ export { default as en } from "./en/translation.json";
 export { default as pt } from "./pt/translation.json";
 export { default as es } from "./es/translation.json";
 export { default as fr } from "./fr/translation.json";
+export { default as hu } from "./hu/translation.json";
