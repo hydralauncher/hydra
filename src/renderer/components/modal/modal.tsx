987519fe8dc65fe830419f363bc3e080813424f0
Fernando Zanutto <fernandozanutto@outlook.com>
2024-04-23 19:35:48 -0300

    trigger pipeline

--- src/renderer/components/modal/modal.tsx
+++ src/renderer/components/modal/modal.tsx
@@ -33,7 +33,6 @@ export function Modal({
       if (time - zero <= 400) {
         requestAnimationFrame(animateClosing);
       } else {
-        
         onClose();
         setIsClosing(false);
       }
