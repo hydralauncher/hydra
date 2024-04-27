6b42a5798849d5003827cc6b8609f2920be60f91
Zamitto <nandoaqws@gmail.com>
2024-04-26 23:02:21 -0300

    migrate postinstall from py to js

--- postinstall.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import platform
-import os
-
-if platform.system() == "Windows":
-  if not os.path.exists("resources/dist"):
-    os.mkdir("resources/dist")
-
-  shutil.copy("node_modules/ps-list/vendor/fastlist-0.3.0-x64.exe", "resources/dist/fastlist.exe")
