15edc1698af3f51d2d5d9adeda1df9f96f84e424
Zamitto <nandoaqws@gmail.com>
2024-04-27 20:43:34 -0300

    fix possibility of 404 on home if getRandomGame has no validy elements to return
    this can happen when user has no internet connection when opening hydra

--- src/main/services/steam-250.ts
+++ src/main/services/steam-250.ts
@@ -7,22 +7,25 @@ export interface Steam250Game {
 }
 
 export const requestSteam250 = async (path: string) => {
-  return axios.get(`https://steam250.com${path}`).then((response) => {
-    const { window } = new JSDOM(response.data);
-    const { document } = window;
+  return axios
+    .get(`https://steam250.com${path}`)
+    .then((response) => {
+      const { window } = new JSDOM(response.data);
+      const { document } = window;
 
-    return Array.from(document.querySelectorAll(".appline .title a"))
-      .map(($title: HTMLAnchorElement) => {
-        const steamGameUrl = $title.href;
-        if (!steamGameUrl) return null;
+      return Array.from(document.querySelectorAll(".appline .title a"))
+        .map(($title: HTMLAnchorElement) => {
+          const steamGameUrl = $title.href;
+          if (!steamGameUrl) return null;
 
-        return {
-          title: $title.textContent,
-          objectID: steamGameUrl.split("/").pop(),
-        } as Steam250Game;
-      })
-      .filter((game) => game != null);
-  });
+          return {
+            title: $title.textContent,
+            objectID: steamGameUrl.split("/").pop(),
+          } as Steam250Game;
+        })
+        .filter((game) => game != null);
+    })
+    .catch((_) => []);
 };
 
 const steam250Paths = [
