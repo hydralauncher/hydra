a4df6e9babe75bff40746f7fc562adc0815411a3
Hydra <hydralauncher@proton.me>
2024-04-28 05:59:06 +0100

    feat: adding tray icon

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -8,14 +8,21 @@ import {
 } from "electron-vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import svgr from "vite-plugin-svgr";
-
 export default defineConfig(({ mode }) => {
   loadEnv(mode);
+
+  const sentryPlugin = sentryVitePlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "hydra-launcher",
+    project: "hydra-launcher",
+  });
 
   return {
     main: {
       build: {
+        sourcemap: true,
         rollupOptions: {
           external: ["better-sqlite3"],
         },
@@ -26,19 +33,33 @@ export default defineConfig(({ mode }) => {
           "@locales": resolve("src/locales"),
         },
       },
-      plugins: [externalizeDepsPlugin(), swcPlugin(), bytecodePlugin()],
+      plugins: [
+        externalizeDepsPlugin(),
+        swcPlugin(),
+        bytecodePlugin(),
+        sentryPlugin,
+      ],
     },
     preload: {
       plugins: [externalizeDepsPlugin()],
     },
     renderer: {
+      build: {
+        sourcemap: true,
+      },
       resolve: {
         alias: {
           "@renderer": resolve("src/renderer/src"),
           "@locales": resolve("src/locales"),
         },
       },
-      plugins: [svgr(), react(), vanillaExtractPlugin(), bytecodePlugin()],
+      plugins: [
+        svgr(),
+        react(),
+        vanillaExtractPlugin(),
+        bytecodePlugin(),
+        sentryPlugin,
+      ],
     },
   };
 });
