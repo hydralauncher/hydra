6bb22655e850208d6dbf5aab81724c3e22125cbc
lilezek <devezek@gmail.com>
2024-04-29 20:54:27 +0200

    feat: added support to add magnets and download from real debrid

---
+++ src/main/services/donwloaders/real-debrid.d.ts
@@ -0,0 +1,65 @@
+interface RealDebridUnrestrictLink {
+    id:         string;
+    filename:   string;
+    mimeType:   string;
+    filesize:   number;
+    link:       string;
+    host:       string;
+    host_icon:  string;
+    chunks:     number;
+    crc:        number;
+    download:   string;
+    streamable: number;
+}
+
+interface RealDebridAddMagnet {
+    "id": string,
+    // URL of the created ressource
+    "uri": string
+}
+
+interface RealDebridTorrentInfo {
+    "id": string,
+    "filename": string,
+    "original_filename": string, // Original name of the torrent
+    "hash": string, // SHA1 Hash of the torrent
+    "bytes": number, // Size of selected files only
+    "original_bytes": number, // Total size of the torrent
+    "host": string, // Host main domain
+    "split": number, // Split size of links
+    "progress": number, // Possible values: 0 to 100
+    "status": "downloaded", // Current status of the torrent: magnet_error, magnet_conversion, waiting_files_selection, queued, downloading, downloaded, error, virus, compressing, uploading, dead
+    "added": string, // jsonDate
+    "files": [
+        {
+            "id": number,
+            "path": string, // Path to the file inside the torrent, starting with "/"
+            "bytes": number,
+            "selected": number // 0 or 1
+        },
+        {
+            "id": number,
+            "path": string, // Path to the file inside the torrent, starting with "/"
+            "bytes": number,
+            "selected": number // 0 or 1
+        }
+    ],
+    "links": [
+        "string" // Host URL
+    ],
+    "ended": string, // !! Only present when finished, jsonDate
+    "speed": number, // !! Only present in "downloading", "compressing", "uploading" status
+    "seeders": number // !! Only present in "downloading", "magnet_conversion" status
+}
+
+declare module 'real-debrid-api' {
+    interface Torrent {
+        addMagnet(magnet: string): Promise<RealDebridAddMagnet>;
+        info(id: string): Promise<RealDebridTorrentInfo[]>;
+    }
+
+    export default class {
+        constructor(token: string);
+        torrents: Torrent;
+    }
+}
