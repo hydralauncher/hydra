ec793db7f5ad47a96580c0fac81735ee5f8c93e3
x <gylli251@gmail.com>
2024-05-02 09:05:52 +0000

    requested changes fixed.

--- src/main/events/library/open-game.ts
+++ src/main/events/library/open-game.ts
@@ -2,17 +2,15 @@ import { exec as execCallback } from 'child_process';
 import { gameRepository } from "@main/repository";
 import { registerEvent } from "../register-event";
 import { shell } from "electron";
-import { promisify } from 'util';
-import os from 'os';
+import { promisify } from 'node:util';
+import { logger } from "@main/services";
 
 const exec = promisify(execCallback);
 
 const escapeShellPath = (path: string) => {
-  if (os.platform() === 'win32') {
-    // Windows paths should be enclosed in quotes if they contain spaces
+  if (process.platform === 'win32') {
     return `"${path}"`;
   } else {
-    // Escapes spaces, parentheses for commands in Linux
     return path.replace(/ /g, '\\ ').replace(/\(/g, '\\(').replace(/\)/g, '\\)');
   }
 };
@@ -25,25 +23,22 @@ const openGame = async (
   try {
     await gameRepository.update({ id: gameId }, { executablePath });
 
-    // Check for Lutris only on Linux systems
-    if (os.platform() === 'linux') {
+    if (process.platform === 'linux') {
       const { stdout: lutrisPath } = await exec('which lutris');
       if (lutrisPath.trim()) {
-        // Lutris is installed
-        await exec(`chmod +x "${executablePath}"`); // Make executable only if Lutris is found
+        await exec(`chmod +x "${executablePath}"`);
         const escapedPath = escapeShellPath(executablePath);
         try {
-          await exec(`lutris -e "${escapedPath}"`); // Use Lutris to open the game
+          await exec(`lutris -e "${escapedPath}"`);
         } catch (err) {
-          console.error('Error launching with Lutris:', err);
+          logger.error('Error launching with Lutris:', err);
         }
       }
     } else {
-      // For non-Linux systems, just open the path
       shell.openPath(executablePath);
     }
   } catch (err) {
-    console.error('Failed operation:', err);
+    logger.error('Failed operation:', err);
   }
 };
 
