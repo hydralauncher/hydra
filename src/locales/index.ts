68f5dd16ae04c2064df924c0c380f0c4c08ae189
FerNikoMF <ferdasuper1230@gmail.com>
2024-05-02 13:17:42 +0500

    added Russian language

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -4,3 +4,4 @@ export { default as es } from "./es/translation.json";
 export { default as fr } from "./fr/translation.json";
 export { default as hu } from "./hu/translation.json";
 export { default as it } from "./it/translation.json";
+export { default as ru } from "./ru/translation.json";
