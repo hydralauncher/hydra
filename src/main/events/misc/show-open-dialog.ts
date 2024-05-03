80eafb52074c87e18b147a09a725a7b5cb8a0d6a
ArjixWasTaken <me@arjix.dev>
2024-05-03 22:20:33 +0300

    all my homies hate tsc /j

--- src/main/events/misc/show-open-dialog.ts
+++ src/main/events/misc/show-open-dialog.ts
@@ -4,10 +4,8 @@ import { registerEvent } from "../register-event";
 
 const showOpenDialog = async (
   _event: Electron.IpcMainInvokeEvent,
-  options: Electron.OpenDialogOptions,
-) =>
-  WindowManager.mainWindow &&
-  dialog.showOpenDialog(WindowManager.mainWindow, options);
+  options: Electron.OpenDialogOptions
+) => dialog.showOpenDialog(WindowManager.mainWindow!, options);
 
 registerEvent(showOpenDialog, {
   name: "showOpenDialog",
