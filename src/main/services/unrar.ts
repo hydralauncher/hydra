101fd7b6914fa5eebe1604fc47b21ea1f8bfda43
lilezek <devezek@gmail.com>
2024-05-04 19:27:14 +0200

    fix: copy unrar.wasm as part of the postinstallation process

--- src/main/services/unrar.ts
+++ src/main/services/unrar.ts
@@ -1,12 +1,18 @@
 import { Extractor, createExtractorFromFile } from "node-unrar-js";
 import fs from "node:fs";
+import path from "node:path";
+import { app } from "electron";
+
+const wasmPath = app.isPackaged
+  ? path.join(process.resourcesPath, "unrar.wasm")
+  : path.join(__dirname, "..", "..", "unrar.wasm");
 
 const wasmBinary = fs.readFileSync(
-  require.resolve("node-unrar-js/esm/js/unrar.wasm")
+  require.resolve(wasmPath)
 );
 
 export class Unrar {
-  private constructor(private extractor: Extractor<Uint8Array>) {}
+  private constructor(private extractor: Extractor<Uint8Array>) { }
 
   static async fromFilePath(filePath: string, targetFolder: string) {
     const extractor = await createExtractorFromFile({
