b8b251f48e0bd603d374e6c311d278b80d1705ac
cardoso <jefersoncardoso.dev@gmail.com>
2024-05-09 18:01:36 -0400

    Standardizing the code and adding a feature to enhance performance
    Using the Singleton pattern will ensure that only one instance of the Session class is created.
    Replacing threading with asyncio now makes it asynchronous and will execute multiple asynchronous tasks on a single thread and WILL NOT
    have a large memory consumption.

--- torrent-client/main.py
+++ torrent-client/main.py
@@ -1,15 +1,27 @@
-import libtorrent as lt
-import sys
-from fifo import Fifo
+import asyncio
 import json
-import threading
+import sys
 import time
+
+import libtorrent as lt
+from fifo import Fifo
 
 torrent_port = sys.argv[1]
 read_sock_path = sys.argv[2]
 write_sock_path = sys.argv[3]
 
-session = lt.session({'listen_interfaces': '0.0.0.0:{port}'.format(port=torrent_port)})
+class SessionSingleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.session = lt.session({'listen_interfaces': '0.0.0.0:{port}'.format(port=torrent_port)})
+        return cls._instance
+
+    def get_session(self):
+        return self.session
+
 read_fifo = Fifo(read_sock_path)
 write_fifo = Fifo(write_sock_path)
 
@@ -24,40 +36,16 @@ def get_eta(status):
     else:
         return 1
 
-def start_download(game_id: int, magnet: str, save_path: str):
+async def get_download_updates():
     global torrent_handle
     global downloading_game_id
 
-    params = {'url': magnet, 'save_path': save_path}
-    torrent_handle = session.add_torrent(params)
-    downloading_game_id = game_id
-    torrent_handle.set_flags(lt.torrent_flags.auto_managed)
-    torrent_handle.resume()
-
-def pause_download():
-    global downloading_game_id
-
-    if torrent_handle:
-        torrent_handle.pause()
-        torrent_handle.unset_flags(lt.torrent_flags.auto_managed)
-        downloading_game_id = 0
-
-def cancel_download():
-    global downloading_game_id
-    global torrent_handle
-
-    if torrent_handle:
-        torrent_handle.pause()
-        session.remove_torrent(torrent_handle)
-        torrent_handle = None
-        downloading_game_id = 0
-
-def get_download_updates():
     while True:
         if downloading_game_id == 0:
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
             continue
 
+        session = SessionSingleton().get_session()
         status = torrent_handle.status()
         info = torrent_handle.get_torrent_info()
 
@@ -77,9 +65,12 @@ def get_download_updates():
         if status.progress == 1:
             cancel_download()
 
-        time.sleep(0.5)
+        await asyncio.sleep(0.5)
 
-def listen_to_socket():
+async def listen_to_socket():
+    global torrent_handle
+    global downloading_game_id
+
     while True:
         msg = read_fifo.recv(1024 * 2)
         payload = json.loads(msg.decode("utf-8"))
@@ -95,9 +86,35 @@ def listen_to_socket():
         if payload['action'] == "cancel":
             cancel_download()
 
-if __name__ == "__main__":
-    p1 = threading.Thread(target=get_download_updates)
-    p2 = threading.Thread(target=listen_to_socket)
+async def start_download(game_id: int, magnet: str, save_path: str):
+    global torrent_handle
+    global downloading_game_id
 
-    p1.start()
-    p2.start()
+    session = SessionSingleton().get_session()
+    params = {'url': magnet, 'save_path': save_path}
+    torrent_handle = session.add_torrent(params)
+    downloading_game_id = game_id
+    torrent_handle.set_flags(lt.torrent_flags.auto_managed)
+    torrent_handle.resume()
+
+async def pause_download():
+    global downloading_game_id
+
+    if torrent_handle:
+        torrent_handle.pause()
+        torrent_handle.unset_flags(lt.torrent_flags.auto_managed)
+        downloading_game_id = 0
+
+async def cancel_download():
+    global downloading_game_id
+    global torrent_handle
+
+    if torrent_handle:
+        torrent_handle.pause()
+        session = SessionSingleton().get_session()
+        session.remove_torrent(torrent_handle)
+        torrent_handle = None
+        downloading_game_id = 0
+
+if __name__ == "__main__":
+    asyncio.run(asyncio.gather(get_download_updates(), listen_to_socket()))
