4cf81bfdc3ed6737b991bb22268ffc02946a2c43
ChristoferMendes <christoferluizdsm@gmail.com>
2024-05-10 12:09:59 -0300

    feat: Add tooltip component with styles and visibility logic

---
+++ src/renderer/src/components/tooltip/tooltip.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import * as styles from './tooltip.css'
+
+interface TooltipProps {
+  children: React.ReactNode;
+  tooltipText: string;
+}
+
+export function Tooltip({ children, tooltipText }: Readonly<TooltipProps>) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+    className={styles.tooltipStyle}
+    onMouseEnter={() => setIsVisible(true)}
+    onMouseLeave={() => setIsVisible(false)}
+  >
+    {children}
+    <span className={`${styles.tooltipTextStyle} ${isVisible ? styles.tooltipVisible : ''}`}>{tooltipText}</span>
+  </div>
+  );
+}
