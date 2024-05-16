a2f2c2aec1d0c0188d0eca8247de48f836a59968
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-16 20:35:51 -0300

    change migration so it sets uploadDate = createdAt

--- src/main/migrations/1715900413313-fix_repack_uploadDate.ts
+++ src/main/migrations/1715900413313-fix_repack_uploadDate.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class FixRepackUploadDate1715900413313 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `UPDATE repack SET uploadDate = datetime('now') WHERE uploadDate LIKE '%NaN%';`
+      `UPDATE repack SET uploadDate = createdAt WHERE uploadDate LIKE '%NaN%';`
     );
   }
 
