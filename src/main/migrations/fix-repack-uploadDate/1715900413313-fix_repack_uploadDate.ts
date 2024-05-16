be4d13533c6dc251c3b7df886e0bb7825766e933
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-16 20:30:08 -0300

    fix: add migration

---
+++ src/main/migrations/fix-repack-uploadDate/1715900413313-fix_repack_uploadDate.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class FixRepackUploadDate1715900413313 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `UPDATE repack SET uploadDate = datetime('now') WHERE uploadDate LIKE '%NaN%';`
+    );
+  }
+
+  public async down(_: QueryRunner): Promise<void> {
+    return;
+  }
+}
---
+++ src/main/migrations/index.ts
@@ -0,0 +1,3 @@
+import { FixRepackUploadDate1715900413313 } from "./fix-repack-uploadDate/1715900413313-fix_repack_uploadDate";
+
+export default [FixRepackUploadDate1715900413313];
