484e79dba3a1458894389797f28f5bdf5e7e51d0
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-18 22:29:11 -0300

    WIP: showing splash screen

---
+++ src/renderer/src/splash.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Splash from "./pages/splash/splash";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Splash />
+  </React.StrictMode>
+);
