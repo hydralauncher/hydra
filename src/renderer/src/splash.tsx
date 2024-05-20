977f757ee28051ec98e2b98465a006a09f4dfd64
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-20 15:52:40 -0300

    remove un needed changes

--- src/renderer/src/splash.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Splash from "./pages/splash/splash";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Splash />
-  </React.StrictMode>
-);
