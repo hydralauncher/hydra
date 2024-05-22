c162d44f80fd944e2d53ea5ef57098fa83506a2b
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-22 12:03:39 -0300

    feat: replace log lib

--- src/main/services/logger.ts
+++ src/main/services/logger.ts
@@ -1,11 +1,29 @@
-import winston from "winston";
+import { app } from "electron";
+import log from "electron-log";
+import path from "path";
 
-export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "info.log", level: "info" }),
-    new winston.transports.File({ filename: "combined.log" }),
-  ],
+const logsPath = app.isPackaged
+  ? path.join(app.getAppPath(), "..", "..", "logs")
+  : path.join(app.getAppPath(), "logs");
+
+log.transports.file.resolvePathFn = (
+  _: log.PathVariables,
+  message?: log.LogMessage | undefined
+) => {
+  switch (message?.level) {
+    case "error":
+      return path.join(logsPath, "error.txt");
+    case "info":
+      return path.join(logsPath, "info.txt");
+    default:
+      return path.join(logsPath, "logs.txt");
+  }
+};
+
+log.errorHandler.startCatching({
+  showDialog: false,
 });
+
+log.initialize();
+
+export const logger = log.scope("main");
