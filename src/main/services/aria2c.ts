79793bbb0688c69ca16cecac78cd3e2af42d22e7
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-30 13:44:36 -0300

    remove debug log

--- src/main/services/aria2c.ts
+++ src/main/services/aria2c.ts
@@ -1,14 +1,11 @@
 import path from "node:path";
 import { spawn } from "node:child_process";
 import { app } from "electron";
-import { logger } from "./logger";
 
 export const startAria2 = () => {
   const binaryPath = app.isPackaged
     ? path.join(process.resourcesPath, "aria2", "aria2c")
     : path.join(__dirname, "..", "..", "aria2", "aria2c");
-
-  logger.info("startAria2 on:", binaryPath);
 
   return spawn(
     binaryPath,
