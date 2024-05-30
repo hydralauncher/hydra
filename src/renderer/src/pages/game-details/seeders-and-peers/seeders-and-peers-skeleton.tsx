8848dc439c5d8745f99b17146317f56a23bbd618
ChristoferMendes <christoferluizdsm@gmail.com>
2024-05-29 23:59:08 -0300

    fix(seeders-and-peers-skeleton): Fix layout issue in skeleton component

--- src/renderer/src/pages/game-details/seeders-and-peers/seeders-and-peers-skeleton.tsx
+++ src/renderer/src/pages/game-details/seeders-and-peers/seeders-and-peers-skeleton.tsx
@@ -1,20 +1,23 @@
-import Skeleton from "react-loading-skeleton";
+import { vars } from "@renderer/theme.css";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 export function SeedersAndPeersSkeleton() {
   return (
-    <div
-      style={{
-        display: "flex",
-      }}
-    >
-      <Skeleton width={40} height={20} />
-      <Skeleton
-        width={40}
-        height={20}
+    <SkeletonTheme baseColor={vars.color.background} highlightColor="#444">
+      <div
         style={{
-          marginLeft: "12px",
+          display: "flex",
         }}
-      />
-    </div>
+      >
+        <Skeleton width={40} height={20} />
+        <Skeleton
+          width={40}
+          height={20}
+          style={{
+            marginLeft: "12px",
+          }}
+        />
+      </div>
+    </SkeletonTheme>
   );
 }
