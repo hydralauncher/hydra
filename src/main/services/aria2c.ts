d517e08413c8655d6d6d6a56e98046e636d8fc11
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-05-30 21:35:38 -0300

    more logs

--- src/main/services/aria2c.ts
+++ src/main/services/aria2c.ts
@@ -10,14 +10,17 @@ export const startAria2 = () => {
 
   logger.log("starting aria2 at:", binaryPath);
 
-  return spawn(
-    binaryPath,
-    [
-      "--enable-rpc",
-      "--rpc-listen-all",
-      "--file-allocation=none",
-      "--allow-overwrite=true",
-    ],
-    { stdio: "inherit", windowsHide: true }
-  );
+  const cp = spawn(binaryPath, [
+    "--enable-rpc",
+    "--rpc-listen-all",
+    "--file-allocation=none",
+    "--allow-overwrite=true",
+  ]);
+
+  cp.stdout?.on("data", async (data) => {
+    const msg = Buffer.from(data).toString("utf-8");
+    logger.info(msg);
+  });
+
+  return cp;
 };
