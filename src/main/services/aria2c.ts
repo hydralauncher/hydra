b7a61f4567a226d0839fa88b4f75fb8f67e6b75a
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-06-06 12:14:08 -0300

    remove log and undo release change

--- src/main/services/aria2c.ts
+++ src/main/services/aria2c.ts
@@ -1,26 +1,20 @@
 import path from "node:path";
 import { spawn } from "node:child_process";
 import { app } from "electron";
-import { logger } from "./logger";
 
 export const startAria2 = () => {
   const binaryPath = app.isPackaged
     ? path.join(process.resourcesPath, "aria2", "aria2c")
     : path.join(__dirname, "..", "..", "aria2", "aria2c");
 
-  logger.log("starting aria2 at:", binaryPath);
-
-  const cp = spawn(binaryPath, [
-    "--enable-rpc",
-    "--rpc-listen-all",
-    "--file-allocation=none",
-    "--allow-overwrite=true",
-  ]);
-
-  cp.stdout?.on("data", async (data) => {
-    const msg = Buffer.from(data).toString("utf-8");
-    logger.info(msg);
-  });
-
-  return cp;
+  return spawn(
+    binaryPath,
+    [
+      "--enable-rpc",
+      "--rpc-listen-all",
+      "--file-allocation=none",
+      "--allow-overwrite=true",
+    ],
+    { stdio: "inherit", windowsHide: true }
+  );
 };
