e66d7ce56672fbfd74a8d6ec7ca44f6507a5377f
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-06-16 23:43:18 -0300

    feat: batch games and code refactor

---
+++ src/main/services/library-sync/upload-batch-games.ts
@@ -0,0 +1,35 @@
+import { gameRepository } from "@main/repository";
+import { chunk } from "lodash-es";
+import { IsNull } from "typeorm";
+import { HydraApi } from "../hydra-api";
+import { logger } from "../logger";
+import { AxiosError } from "axios";
+
+export const uploadBatchGames = async () => {
+  try {
+    const games = await gameRepository.find({
+      where: { remoteId: IsNull(), isDeleted: false },
+    });
+
+    const gamesChunks = chunk(games, 200);
+    for (const chunk of gamesChunks) {
+      await HydraApi.post(
+        "/games/batch",
+        chunk.map((game) => {
+          return {
+            objectId: game.objectID,
+            playTimeInMilliseconds: Math.round(game.playTimeInMilliseconds),
+            shop: game.shop,
+            lastTimePlayed: game.lastTimePlayed,
+          };
+        })
+      );
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      logger.error("uploadBatchGames", err.response, err.message);
+    } else {
+      logger.error("uploadBatchGames", err);
+    }
+  }
+};
