05e4934f9f3b02f72bf7cba9f3380b31edd2fce0
Chubby Granny Chaser <hydralauncher@proton.me>
2024-06-28 21:47:27 +0100

    ci: fix sentry variable

--- src/main/events/auth/open-auth-window.ts
+++ src/main/events/auth/open-auth-window.ts
@@ -1,7 +1,8 @@
 import { registerEvent } from "../register-event";
 import { WindowManager } from "@main/services";
 
-const openAuthWindow = async (_event: Electron.IpcMainInvokeEvent) =>
-  WindowManager.openAuthWindow();
+const openAuthWindow = async (_event: Electron.IpcMainInvokeEvent) => {
+  throw new Error("sentry");
+};
 
 registerEvent("openAuthWindow", openAuthWindow);
