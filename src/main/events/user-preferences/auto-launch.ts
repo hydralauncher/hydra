89e8629ea415143757a789d11c3baf7e5bb09eca
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-06-29 16:48:55 -0300

    fix: now creating vbs script on runtime

--- src/main/events/user-preferences/auto-launch.ts
+++ src/main/events/user-preferences/auto-launch.ts
@@ -5,6 +5,13 @@ import { app } from "electron";
 import fs from "node:fs";
 import path from "node:path";
 
+const getVbsFileContent = (exePath: string) => {
+  return `Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run """${exePath}""", 0 'Must quote command if it has spaces; must escape quotes
+Set WshShell = Nothing
+`;
+};
+
 const autoLaunch = async (
   _event: Electron.IpcMainInvokeEvent,
   enabled: boolean
@@ -19,9 +26,8 @@ const autoLaunch = async (
     const destination = path.join(windowsStartupPath, "Hydra.vbs");
 
     if (enabled) {
-      const scriptPath = path.join(process.resourcesPath, "hydralauncher.vbs");
-
-      fs.copyFileSync(scriptPath, destination);
+      const vbsFileContent = getVbsFileContent(app.getPath("exe"));
+      fs.writeFileSync(destination, vbsFileContent);
     } else {
       appLauncher.disable().catch();
       fs.rmSync(destination);
