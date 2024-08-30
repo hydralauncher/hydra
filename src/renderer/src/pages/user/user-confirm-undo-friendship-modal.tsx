40d2b36a20461ef7dc1a9a391ba35dab6074312d
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-08-30 10:06:10 -0300

    feat: remove unnecessary react fragment

--- src/renderer/src/pages/user/user-confirm-undo-friendship-modal.tsx
+++ src/renderer/src/pages/user/user-confirm-undo-friendship-modal.tsx
@@ -18,25 +18,23 @@ export const UserConfirmUndoFriendshipModal = ({
   const { t } = useTranslation("user_profile");
 
   return (
-    <>
-      <Modal
-        visible={visible}
-        title={t("sign_out_modal_title")}
-        onClose={onClose}
-      >
-        <div className={styles.signOutModalContent}>
-          <p>{t("undo_friendship_modal_text", { displayName })}</p>
-          <div className={styles.signOutModalButtonsContainer}>
-            <Button onClick={onConfirm} theme="danger">
-              {t("undo_friendship")}
-            </Button>
+    <Modal
+      visible={visible}
+      title={t("sign_out_modal_title")}
+      onClose={onClose}
+    >
+      <div className={styles.signOutModalContent}>
+        <p>{t("undo_friendship_modal_text", { displayName })}</p>
+        <div className={styles.signOutModalButtonsContainer}>
+          <Button onClick={onConfirm} theme="danger">
+            {t("undo_friendship")}
+          </Button>
 
-            <Button onClick={onClose} theme="primary">
-              {t("cancel")}
-            </Button>
-          </div>
+          <Button onClick={onClose} theme="primary">
+            {t("cancel")}
+          </Button>
         </div>
-      </Modal>
-    </>
+      </div>
+    </Modal>
   );
 };
