48e627f94566be51bea77d02247441f4f7f4f970
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-09-12 11:12:36 -0300

    feat: searching from api

--- src/main/events/catalogue/search-games.ts
+++ src/main/events/catalogue/search-games.ts
@@ -1,10 +1,24 @@
 import { registerEvent } from "../register-event";
-import { searchSteamGames } from "../helpers/search-games";
+import { getSteamGameById } from "../helpers/search-games";
 import { CatalogueEntry } from "@types";
+import { HydraApi } from "@main/services";
 
 const searchGamesEvent = async (
   _event: Electron.IpcMainInvokeEvent,
   query: string
-): Promise<CatalogueEntry[]> => searchSteamGames({ query, limit: 12 });
+): Promise<CatalogueEntry[]> => {
+  const games = await HydraApi.get<
+    { objectId: string; title: string; shop: string }[]
+  >("/games/search", { title: query, take: 12, skip: 0 }, { needsAuth: false });
+
+  const steamGames = await Promise.all(
+    games.map((game) => getSteamGameById(game.objectId))
+  );
+  const filteredGames = steamGames.filter(
+    (game) => game !== null
+  ) as CatalogueEntry[];
+
+  return filteredGames;
+};
 
 registerEvent("searchGames", searchGamesEvent);
