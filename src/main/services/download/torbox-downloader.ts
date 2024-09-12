d48cb767cb7ec47efea03ef51d1d6dba4f098ed4
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2024-09-11 22:13:11 -0500

    feat: gets download status

--- src/main/services/download/torbox-downloader.ts
+++ src/main/services/download/torbox-downloader.ts
@@ -2,9 +2,88 @@ import { Game } from "@main/entity";
 import { TorBoxClient } from "../torbox";
 import { HttpDownload } from "./http-download";
 import { GenericHttpDownloader } from "./generic-http-downloader";
+import { gameRepository } from "@main/repository";
+// import { calculateETA } from "./helpers";
+import { DownloadProgress } from "@types";
 
 export class TorBoxDownloader extends GenericHttpDownloader {
   private static torboxTorrentId: number | null = null;
+
+  public static async getStatus() {
+    if (this.downloadingGame) {
+      const download = this.downloads.get(this.downloadingGame.id)!;
+      const status = download.getStatus();
+
+      if (status) {
+        var progress =
+          Number(status.completedLength) / Number(status.totalLength) || 0;
+
+        if (progress === Infinity) {
+          progress = 0;
+        }
+
+        await gameRepository.update(
+          { id: this.downloadingGame!.id },
+          {
+            bytesDownloaded: Number(status.completedLength),
+            fileSize: Number(status.totalLength),
+            progress,
+            status: "active",
+            folderName: status.folderName,
+          }
+        );
+
+        const result = {
+          numPeers: 0,
+          numSeeds: 0,
+          downloadSpeed: status.downloadSpeed,
+          timeRemaining: -1,
+          isDownloadingMetadata: false,
+          isCheckingFiles: false,
+          progress,
+          gameId: this.downloadingGame!.id,
+        } as DownloadProgress;
+
+        if (progress === 1) {
+          this.downloads.delete(this.downloadingGame.id);
+          this.downloadingGame = null;
+        }
+
+        return result;
+      }
+    }
+
+    return null;
+  }
+
+  static async pauseDownload() {
+    if (this.downloadingGame) {
+      const httpDownload = this.downloads.get(this.downloadingGame!.id!);
+
+      if (httpDownload) {
+        await httpDownload.pauseDownload();
+      }
+
+      this.downloadingGame = null;
+    }
+  }
+
+  static async cancelDownload(gameId: number) {
+    const httpDownload = this.downloads.get(gameId);
+
+    if (httpDownload) {
+      await httpDownload.cancelDownload();
+      this.downloads.delete(gameId);
+    }
+  }
+
+  static async resumeDownload(gameId: number) {
+    const httpDownload = this.downloads.get(gameId);
+
+    if (httpDownload) {
+      await httpDownload.resumeDownload();
+    }
+  }
 
   private static async getTorBoxDownloadUrl() {
     if (this.torboxTorrentId) {
