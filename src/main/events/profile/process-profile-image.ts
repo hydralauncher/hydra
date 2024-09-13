e8515e199084b094c0966bed226c564d8964dbca
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-09-13 17:22:29 -0300

    feat: process profile image

--- src/main/events/profile/process-profile-image.ts
+++ src/main/events/profile/process-profile-image.ts
@@ -5,7 +5,7 @@ const processProfileImage = async (
   _event: Electron.IpcMainInvokeEvent,
   path: string
 ) => {
-  return await PythonInstance.processProfileImage(path);
+  return PythonInstance.processProfileImage(path);
 };
 
 registerEvent("processProfileImage", processProfileImage);
