6fbd4bd9fed3d019abb05d54d4e06dc884f79f25
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-09-13 19:45:37 -0300

    feat: add language migration

--- src/main/knex-client.ts
+++ src/main/knex-client.ts
@@ -2,12 +2,13 @@ import knex, { Knex } from "knex";
 import { databasePath } from "./constants";
 import { Hydra2_0_3 } from "./migrations/20240830143811_Hydra_2_0_3";
 import { RepackUris } from "./migrations/20240830143906_RepackUris";
+import { UpdateUserLanguage } from "./migrations/20240913213944_update_user_language";
 
 export type HydraMigration = Knex.Migration & { name: string };
 
 class MigrationSource implements Knex.MigrationSource<HydraMigration> {
   getMigrations(): Promise<HydraMigration[]> {
-    return Promise.resolve([Hydra2_0_3, RepackUris]);
+    return Promise.resolve([Hydra2_0_3, RepackUris, UpdateUserLanguage]);
   }
   getMigrationName(migration: HydraMigration): string {
     return migration.name;
