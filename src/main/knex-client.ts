f9906bfe9571d39bb4f1b66f7478e08c9c2e0a9e
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-09-15 01:00:44 -0300

    fix: message and migration

--- src/main/knex-client.ts
+++ src/main/knex-client.ts
@@ -3,12 +3,18 @@ import { databasePath } from "./constants";
 import { Hydra2_0_3 } from "./migrations/20240830143811_Hydra_2_0_3";
 import { RepackUris } from "./migrations/20240830143906_RepackUris";
 import { UpdateUserLanguage } from "./migrations/20240913213944_update_user_language";
+import { EnsureRepackUris } from "./migrations/20240915035339_ensure_repack_uris";
 
 export type HydraMigration = Knex.Migration & { name: string };
 
 class MigrationSource implements Knex.MigrationSource<HydraMigration> {
   getMigrations(): Promise<HydraMigration[]> {
-    return Promise.resolve([Hydra2_0_3, RepackUris, UpdateUserLanguage]);
+    return Promise.resolve([
+      Hydra2_0_3,
+      RepackUris,
+      UpdateUserLanguage,
+      EnsureRepackUris,
+    ]);
   }
   getMigrationName(migration: HydraMigration): string {
     return migration.name;
