aa4ca256533eee8285948c22408057cd3eda921d
Chubby Granny Chaser <hydralauncher@proton.me>
2024-09-17 16:25:19 +0100

    feat: adding flame animation

--- src/renderer/src/pages/home/home.tsx
+++ src/renderer/src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
@@ -8,16 +8,19 @@ import { Button, GameCard, Hero } from "@renderer/components";
 import type { Steam250Game, CatalogueEntry } from "@types";
 
 import starsAnimation from "@renderer/assets/lottie/stars.json";
+import flameAnimation from "@renderer/assets/lottie/flame.json";
 
 import * as styles from "./home.css";
-import { vars } from "@renderer/theme.css";
-import Lottie from "lottie-react";
+import { SPACING_UNIT, vars } from "@renderer/theme.css";
+import Lottie, { type LottieRefCurrentProps } from "lottie-react";
 import { buildGameDetailsPath } from "@renderer/helpers";
 import { CatalogueCategory } from "@shared";
 
 export function Home() {
   const { t } = useTranslation("home");
   const navigate = useNavigate();
+
+  const flameAnimationRef = useRef<LottieRefCurrentProps>(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [randomGame, setRandomGame] = useState<Steam250Game | null>(null);
@@ -82,6 +85,18 @@ export function Home() {
 
   const categories = Object.values(CatalogueCategory);
 
+  const handleMouseEnterCategory = (category: CatalogueCategory) => {
+    if (category === CatalogueCategory.Hot) {
+      flameAnimationRef?.current?.play();
+    }
+  };
+
+  const handleMouseLeaveCategory = (category: CatalogueCategory) => {
+    if (category === CatalogueCategory.Hot) {
+      flameAnimationRef?.current?.stop();
+    }
+  };
+
   return (
     <SkeletonTheme baseColor={vars.color.background} highlightColor="#444">
       <section className={styles.content}>
@@ -100,7 +115,28 @@ export function Home() {
                       : "outline"
                   }
                   onClick={() => handleCategoryClick(category)}
+                  onMouseEnter={() => handleMouseEnterCategory(category)}
+                  onMouseLeave={() => handleMouseLeaveCategory(category)}
                 >
+                  {category === CatalogueCategory.Hot && (
+                    <div
+                      style={{ width: 16, height: 16, position: "relative" }}
+                    >
+                      <Lottie
+                        lottieRef={flameAnimationRef}
+                        animationData={flameAnimation}
+                        loop
+                        autoplay={false}
+                        style={{
+                          width: 30,
+                          top: -10,
+                          left: -5,
+                          position: "absolute",
+                        }}
+                      />
+                    </div>
+                  )}
+
                   {t(category)}
                 </Button>
               </li>
@@ -123,7 +159,25 @@ export function Home() {
           </Button>
         </section>
 
-        <h2>{t(currentCatalogueCategory)}</h2>
+        <h2 style={{ display: "flex", gap: SPACING_UNIT }}>
+          {currentCatalogueCategory === CatalogueCategory.Hot && (
+            <div style={{ width: 24, height: 24, position: "relative" }}>
+              <Lottie
+                animationData={flameAnimation}
+                loop
+                autoplay
+                style={{
+                  width: 40,
+                  top: -10,
+                  left: -5,
+                  position: "absolute",
+                }}
+              />
+            </div>
+          )}
+
+          {t(currentCatalogueCategory)}
+        </h2>
 
         <section className={styles.cards}>
           {isLoading
