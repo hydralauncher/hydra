973358818bd0af43fad7534c76ccf1c3a3a824c9
Chubby Granny Chaser <hydralauncher@proton.me>
2024-09-22 17:44:06 +0100

    feat: adding dexie

--- src/renderer/src/workers/migration.worker.ts
+++ src/renderer/src/workers/migration.worker.ts
@@ -1,43 +1,35 @@
-import { db, downloadSourcesTable, repacksTable } from "@renderer/dexie";
+// import { db, downloadSourcesTable, repacksTable } from "@renderer/dexie";
 import { DownloadSource, GameRepack } from "@types";
 
 export type Payload = [DownloadSource[], GameRepack[]];
 
-self.onmessage = async (event: MessageEvent<Payload>) => {
-  const [downloadSources, gameRepacks] = event.data;
-
-  const downloadSourcesCount = await downloadSourcesTable.count();
-
-  if (downloadSources.length > downloadSourcesCount) {
-    await db.transaction(
-      "rw",
-      downloadSourcesTable,
-      repacksTable,
-      async () => {}
-    );
-  }
-
+self.onmessage = async (_event: MessageEvent<Payload>) => {
+  // const [downloadSources, gameRepacks] = event.data;
+  // const downloadSourcesCount = await downloadSourcesTable.count();
+  // if (downloadSources.length > downloadSourcesCount) {
+  //   await db.transaction(
+  //     "rw",
+  //     downloadSourcesTable,
+  //     repacksTable,
+  //     async () => {}
+  //   );
+  // }
   // if (type === "MIGRATE_DOWNLOAD_SOURCES") {
   //   const dexieDownloadSources = await downloadSourcesTable.count();
-
   //   if (data.length > dexieDownloadSources) {
   //     await downloadSourcesTable.clear();
   //     await downloadSourcesTable.bulkAdd(data);
   //   }
-
   //   self.postMessage("MIGRATE_DOWNLOAD_SOURCES_COMPLETE");
   // }
-
   // if (type === "MIGRATE_REPACKS") {
   //   const dexieRepacks = await repacksTable.count();
-
   //   if (data.length > dexieRepacks) {
   //     await repacksTable.clear();
   //     await repacksTable.bulkAdd(
   //       data.map((repack) => ({ ...repack, uris: JSON.stringify(repack.uris) }))
   //     );
   //   }
-
   //   self.postMessage("MIGRATE_REPACKS_COMPLETE");
   // }
 };
