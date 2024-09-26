bfb32b79f0966b8f00061d73e41bfb67e969e82e
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-09-26 09:30:37 -0300

    feat: use different db for staging build

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -4,7 +4,12 @@ import path from "node:path";
 export const defaultDownloadsPath = app.getPath("downloads");
 
 export const databaseDirectory = path.join(app.getPath("appData"), "hydra");
-export const databasePath = path.join(databaseDirectory, "hydra.db");
+export const databasePath = path.join(
+  databaseDirectory,
+  import.meta.env.MAIN_VITE_API_URL.includes("staging")
+    ? "hydra_test.db"
+    : "hydra.db"
+);
 
 export const logsPath = path.join(app.getPath("appData"), "hydra", "logs");
 
