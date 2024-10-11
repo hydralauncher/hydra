7e257416570d04af2578f58f536e8d0ca7f560af
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-10-11 16:01:15 -0300

    feat: achievements page adjustments

--- src/renderer/src/pages/achievement/achievements.tsx
+++ src/renderer/src/pages/achievement/achievements.tsx
@@ -2,16 +2,15 @@ import { setHeaderTitle } from "@renderer/features";
 import { useAppDispatch, useDate } from "@renderer/hooks";
 import { steamUrlBuilder } from "@shared";
 import type { GameShop, UserAchievement } from "@types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import * as styles from "./achievements.css";
-import {
-  buildGameDetailsPath,
-  formatDownloadProgress,
-} from "@renderer/helpers";
+import { formatDownloadProgress } from "@renderer/helpers";
 import { TrophyIcon } from "@primer/octicons-react";
-import { SPACING_UNIT, vars } from "@renderer/theme.css";
+import { vars } from "@renderer/theme.css";
+
+const HERO_ANIMATION_THRESHOLD = 25;
 
 export function Achievement() {
   const [searchParams] = useSearchParams();
@@ -21,10 +20,14 @@ export function Achievement() {
   const userId = searchParams.get("userId");
   const displayName = searchParams.get("displayName");
 
+  const heroRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isHeaderStuck, setIsHeaderStuck] = useState(false);
+  const [backdropOpactiy, setBackdropOpacity] = useState(1);
+
   const { t } = useTranslation("achievement");
 
   const { format } = useDate();
-  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
 
@@ -42,17 +45,29 @@ export function Achievement() {
 
   useEffect(() => {
     if (title) {
-      dispatch(
-        setHeaderTitle(
-          displayName
-            ? t("user_achievements", {
-                displayName,
-              })
-            : t("your_achievements")
-        )
-      );
+      dispatch(setHeaderTitle(title));
     }
   }, [dispatch, title]);
+
+  const onScroll: React.UIEventHandler<HTMLElement> = (event) => {
+    const heroHeight = heroRef.current?.clientHeight ?? styles.HERO_HEIGHT;
+
+    const scrollY = (event.target as HTMLDivElement).scrollTop;
+    const opacity = Math.max(
+      0,
+      1 - scrollY / (heroHeight - HERO_ANIMATION_THRESHOLD)
+    );
+
+    if (scrollY >= heroHeight && !isHeaderStuck) {
+      setIsHeaderStuck(true);
+    }
+
+    if (scrollY <= heroHeight && isHeaderStuck) {
+      setIsHeaderStuck(false);
+    }
+
+    setBackdropOpacity(opacity);
+  };
 
   if (!objectId || !shop || !title) return null;
 
@@ -62,40 +77,48 @@ export function Achievement() {
 
   const totalAchievementCount = achievements.length;
 
-  const handleClickGame = () => {
-    navigate(
-      buildGameDetailsPath({
-        shop: shop as GameShop,
-        objectId,
-        title,
-      })
-    );
-  };
-
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <button onClick={handleClickGame}>
-          <img
-            src={steamUrlBuilder.libraryHero(objectId)}
-            alt={title}
-            className={styles.headerImage}
+    <div className={styles.wrapper}>
+      <img
+        src={steamUrlBuilder.libraryHero(objectId)}
+        alt={title}
+        className={styles.headerImage}
+      />
+
+      <section
+        ref={containerRef}
+        onScroll={onScroll}
+        className={styles.container}
+      >
+        <div className={styles.header}>
+          <div
+            style={{
+              flex: 1,
+              opacity: Math.min(1, 1 - backdropOpactiy),
+            }}
           />
-        </button>
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            padding: `0 ${SPACING_UNIT * 2}px`,
-          }}
-        >
-          <h1>{title}</h1>
+
+          <img
+            src={steamUrlBuilder.logo(objectId!)}
+            className={styles.gameLogo}
+            alt={title}
+          />
+        </div>
+
+        <div className={styles.panel({ stuck: isHeaderStuck })}>
+          <h1 style={{ fontSize: "1.2em", marginBottom: "8px" }}>
+            {displayName
+              ? t("user_achievements", {
+                  displayName,
+                })
+              : t("your_achievements")}
+          </h1>
           <div
             style={{
               display: "flex",
               justifyContent: "space-between",
               marginBottom: 8,
+              width: "100%",
               color: vars.color.muted,
             }}
           >
@@ -124,29 +147,29 @@ export function Achievement() {
             className={styles.achievementsProgressBar}
           />
         </div>
-      </div>
 
-      <ul className={styles.list}>
-        {achievements.map((achievement, index) => (
-          <li key={index} className={styles.listItem}>
-            <img
-              className={styles.listItemImage({
-                unlocked: achievement.unlocked,
-              })}
-              src={achievement.icon}
-              alt={achievement.displayName}
-              loading="lazy"
-            />
-            <div>
-              <p>{achievement.displayName}</p>
-              <p>{achievement.description}</p>
-              <small>
-                {achievement.unlockTime && format(achievement.unlockTime)}
-              </small>
-            </div>
-          </li>
-        ))}
-      </ul>
+        <ul className={styles.list}>
+          {achievements.map((achievement, index) => (
+            <li key={index} className={styles.listItem}>
+              <img
+                className={styles.listItemImage({
+                  unlocked: achievement.unlocked,
+                })}
+                src={achievement.icon}
+                alt={achievement.displayName}
+                loading="lazy"
+              />
+              <div>
+                <p>{achievement.displayName}</p>
+                <p>{achievement.description}</p>
+                <small>
+                  {achievement.unlockTime && format(achievement.unlockTime)}
+                </small>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
