108b2552b540ca8879aa2e4db2c22b3013f66b94
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-10-16 12:37:44 -0300

    feat: achievements adjustments

--- src/renderer/src/pages/achievements/achievements-skeleton.tsx
+++ src/renderer/src/pages/achievements/achievements-skeleton.tsx
@@ -4,7 +4,7 @@ import * as styles from "./achievements.css";
 export function AchievementsSkeleton() {
   return (
     <div className={styles.container}>
-      <div className={styles.hero}>
+      <div className={styles.heroImage}>
         <Skeleton className={styles.heroImageSkeleton} />
       </div>
       <div className={styles.heroPanelSkeleton}></div>
