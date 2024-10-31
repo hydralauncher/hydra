f5b409baffca57cc676498ead293735acfa4d2f0
Hachi-R <drive8hachi@gmail.com>
2024-10-31 11:39:18 -0300

    refactor: rename migration to be more descriptive

--- src/main/migrations/20241030171454_add_start_minimized_column.ts
+++ src/main/migrations/20241030171454_add_start_minimized_column.ts
@@ -1,8 +1,8 @@
 import type { HydraMigration } from "@main/knex-client";
 import type { Knex } from "knex";
 
-export const QualityOfLife: HydraMigration = {
-  name: "QualityOfLife",
+export const AddStartMinimizedColumn: HydraMigration = {
+  name: "AddStartMinimizedColumn",
   up: (knex: Knex) => {
     return knex.schema.alterTable("user_preferences", (table) => {
       return table.boolean("startMinimized").notNullable().defaultTo(0);
