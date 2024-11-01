f06f2a196eca89a28e8db2519af3e989efc7f11b
Hachi-R <drive8hachi@gmail.com>
2024-10-31 23:02:23 -0300

    feat: add option in user preferences to seed after download completes

---
+++ src/main/migrations/20241101012727_add_seed_after_download_completes_column.ts
@@ -0,0 +1,17 @@
+import type { HydraMigration } from "@main/knex-client";
+import type { Knex } from "knex";
+
+export const AddSeedAfterDownloadCompletesColumn: HydraMigration = {
+  name: "AddSeedAfterDownloadCompletesColumn",
+  up: (knex: Knex) => {
+    return knex.schema.alterTable("user_preferences", (table) => {
+      return table.boolean("seedAfterDownloadCompletes").notNullable().defaultTo(1);
+    });
+  },
+
+  down: async (knex: Knex) => {
+    return knex.schema.alterTable("user_preferences", (table) => {
+      return table.dropColumn("seedAfterDownloadCompletes");
+    });
+  },
+};
