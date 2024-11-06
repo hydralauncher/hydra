e1a5a95ceb0bbd75e1a5e375996c39e687f18303
Hachi-R <drive8hachi@gmail.com>
2024-11-06 20:28:55 -0300

    chore: removed resume torrent after download

--- src/main/services/download/download-manager.ts
+++ src/main/services/download/download-manager.ts
@@ -78,8 +78,6 @@ export class DownloadManager {
               shouldSeed: true,
             });
           }
-
-          this.resumeDownload(game);
         }
 
         const [nextQueueItem] = await downloadQueueRepository.find({
