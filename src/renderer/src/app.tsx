40ec77342509c70bcf3ea941cc91941614429eb0
Hachi-R <drive8hachi@gmail.com>
2024-11-11 16:39:58 -0300

    lint

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -113,7 +113,7 @@ export function App() {
     const unsubscribe = window.electron.onHardDelete(() => {
       updateLibrary();
     });
-    
+
     return () => unsubscribe();
   }, [updateLibrary]);
 
