e76f449ce69d430002308a4d6244dc82f5a4445b
Hachi-R <drive8hachi@gmail.com>
2024-11-12 04:32:08 -0300

    refactor: export with component definition

--- src/renderer/src/components/dropdown-menu/dropdown-menu.tsx
+++ src/renderer/src/components/dropdown-menu/dropdown-menu.tsx
@@ -1,4 +1,4 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import "./dropdown-menu.scss";
 
 export interface DropdownMenuItem {
@@ -20,7 +20,7 @@ interface DropdownMenuProps {
   alignOffset?: number;
 }
 
-export default ({
+export function DropdownMenu({
   children,
   title,
   items,
@@ -29,49 +29,53 @@ export default ({
   loop = true,
   align = "center",
   alignOffset = 0,
-}: DropdownMenuProps) => (
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger asChild>
-      <button aria-label={title}>{children}</button>
-    </DropdownMenu.Trigger>
+}: DropdownMenuProps) {
+  return (
+    <DropdownMenuPrimitive.Root>
+      <DropdownMenuPrimitive.Trigger asChild>
+        <button aria-label={title}>{children}</button>
+      </DropdownMenuPrimitive.Trigger>
 
-    <DropdownMenu.Portal>
-      <DropdownMenu.Content
-        sideOffset={sideOffset}
-        side={side}
-        loop={loop}
-        align={align}
-        alignOffset={alignOffset}
-        className="dropdown-menu__content"
-      >
-        {title && (
-          <DropdownMenu.Group className="dropdown-menu__group">
-            <div className="dropdown-menu__title-bar">{title}</div>
-          </DropdownMenu.Group>
-        )}
-
-        <DropdownMenu.Separator className="dropdown-menu__separator" />
-
-        <DropdownMenu.Group className="dropdown-menu__group">
-          {items.map(
-            (item) =>
-              item.show !== false && (
-                <DropdownMenu.Item
-                  key={item.label}
-                  aria-label={item.label}
-                  onSelect={item.onClick}
-                  className={`dropdown-menu__item ${item.disabled ? "dropdown-menu__item--disabled" : ""}`}
-                  disabled={item.disabled}
-                >
-                  {item.icon && (
-                    <div className="dropdown-menu__item-icon">{item.icon}</div>
-                  )}
-                  {item.label}
-                </DropdownMenu.Item>
-              )
+      <DropdownMenuPrimitive.Portal>
+        <DropdownMenuPrimitive.Content
+          sideOffset={sideOffset}
+          side={side}
+          loop={loop}
+          align={align}
+          alignOffset={alignOffset}
+          className="dropdown-menu__content"
+        >
+          {title && (
+            <DropdownMenuPrimitive.Group className="dropdown-menu__group">
+              <div className="dropdown-menu__title-bar">{title}</div>
+            </DropdownMenuPrimitive.Group>
           )}
-        </DropdownMenu.Group>
-      </DropdownMenu.Content>
-    </DropdownMenu.Portal>
-  </DropdownMenu.Root>
-);
+
+          <DropdownMenuPrimitive.Separator className="dropdown-menu__separator" />
+
+          <DropdownMenuPrimitive.Group className="dropdown-menu__group">
+            {items.map(
+              (item) =>
+                item.show !== false && (
+                  <DropdownMenuPrimitive.Item
+                    key={item.label}
+                    aria-label={item.label}
+                    onSelect={item.onClick}
+                    className={`dropdown-menu__item ${item.disabled ? "dropdown-menu__item--disabled" : ""}`}
+                    disabled={item.disabled}
+                  >
+                    {item.icon && (
+                      <div className="dropdown-menu__item-icon">
+                        {item.icon}
+                      </div>
+                    )}
+                    {item.label}
+                  </DropdownMenuPrimitive.Item>
+                )
+            )}
+          </DropdownMenuPrimitive.Group>
+        </DropdownMenuPrimitive.Content>
+      </DropdownMenuPrimitive.Portal>
+    </DropdownMenuPrimitive.Root>
+  );
+}
