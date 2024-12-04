278e12327a4cb07807840791a4e70568b25c7d31
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-12-04 12:11:14 -0300

    fix: set window.userDetails after login

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -119,8 +119,6 @@ export function App() {
         if (response) {
           updateUserDetails(response);
           syncFriendRequests();
-
-          window["userDetails"] = response;
         }
       })
       .finally(() => {
