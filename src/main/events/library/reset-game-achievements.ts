4e0be56bbd0f935ce08237990f582e4bbf689fba
Hachi-R <drive8hachi@gmail.com>
2024-12-17 13:15:55 -0300

    feat: implement reset game achievements functionality

---
+++ src/main/events/library/reset-game-achievements.ts
@@ -0,0 +1,52 @@
+import { gameAchievementRepository, gameRepository } from "@main/repository";
+import { registerEvent } from "../register-event";
+import { findAchievementFiles } from "@main/services/achievements/find-achivement-files";
+import fs from "fs";
+import { WindowManager } from "@main/services";
+import { getUnlockedAchievements } from "../user/get-unlocked-achievements";
+
+const resetGameAchievements = async (
+  _event: Electron.IpcMainInvokeEvent,
+  gameId: number
+) => {
+  const game = await gameRepository.findOne({ where: { id: gameId } });
+
+  if (!game) return;
+
+  const achievementFiles = findAchievementFiles(game);
+
+  if (achievementFiles.length) {
+    try {
+      await Promise.all(
+        achievementFiles.map(async (achievementFile) => {
+          await fs.promises.rm(achievementFile.filePath, { recursive: true });
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  await gameAchievementRepository.update(
+    { objectId: game.objectID },
+    {
+      unlockedAchievements: null,
+      achievements: null,
+    }
+  );
+
+  // TODO: remove from db
+
+  const gameAchievements = await getUnlockedAchievements(
+    game.objectID,
+    game.shop,
+    false
+  );
+
+  WindowManager.mainWindow?.webContents.send(
+    `on-update-achievements-${game.objectID}-${game.shop}`,
+    gameAchievements
+  );
+};
+
+registerEvent("resetGameAchievements", resetGameAchievements);
