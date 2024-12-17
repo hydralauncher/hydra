4c3eb98041ae56e9f1eb8fe66778e5855852ca4a
JackEnx <167036558+JackEnx@users.noreply.github.com>
2024-12-17 15:48:11 -0300

    refactor: rpc url to a generic url

--- src/main/vite-env.d.ts
+++ src/main/vite-env.d.ts
@@ -6,7 +6,7 @@ interface ImportMetaEnv {
   readonly MAIN_VITE_ANALYTICS_API_URL: string;
   readonly MAIN_VITE_AUTH_URL: string;
   readonly MAIN_VITE_CHECKOUT_URL: string;
-  readonly MAIN_VITE_RPC_GAME_EXECUTABLES_URL: string;
+  readonly MAIN_VITE_EXTERNAL_RESOURCES: string;
 }
 
 interface ImportMeta {
