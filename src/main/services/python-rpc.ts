0ba82521da7a0bd95ab79a2c1527b19748eaf4b1
Hachi-R <drive8hachi@gmail.com>
2024-12-21 15:34:29 -0300

    feat: implement seed process initiation in PythonRPC

--- src/main/services/python-rpc.ts
+++ src/main/services/python-rpc.ts
@@ -8,6 +8,7 @@ import crypto from "node:crypto";
 import { logger } from "./logger";
 import { Readable } from "node:stream";
 import { app, dialog } from "electron";
+import { startSeedProcess } from "./seed";
 
 const binaryNameByPlatform: Partial<Record<NodeJS.Platform, string>> = {
   darwin: "hydra-python-rpc",
@@ -83,6 +84,8 @@ export class PythonRPC {
       this.logStderr(childProcess.stderr);
 
       this.pythonProcess = childProcess;
+
+      startSeedProcess();
     }
   }
 
