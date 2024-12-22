93ef0c256e30c5c1615f671ee902aee6c24c4a5a
Hachi-R <drive8hachi@gmail.com>
2024-12-22 09:06:39 -0300

    feat: updated pauseGameSeed and resumeGameSeed events

--- src/main/events/torrenting/pause-game-seed.ts
+++ src/main/events/torrenting/pause-game-seed.ts
@@ -3,23 +3,22 @@ import { registerEvent } from "../register-event";
 import { DownloadManager } from "@main/services";
 import { dataSource } from "@main/data-source";
 import { Game } from "@main/entity";
-import { gameRepository } from "@main/repository";
+
 
 const pauseGameSeed = async (
   _event: Electron.IpcMainInvokeEvent,
   gameId: number
 ) => {
-  const game = await gameRepository.findOneBy({ id: gameId });
-
-  if (game?.status !== "seeding") return;
-
   await dataSource.transaction(async (transactionalEntityManager) => {
     await transactionalEntityManager
       .getRepository(Game)
-      .update({ id: gameId }, { status: "complete", shouldSeed: false });
+      .update(
+        { id: gameId },
+        { status: "complete", shouldSeed: false }
+      );
   });
 
-  await DownloadManager.pauseSeeding(gameId);
+  await DownloadManager.cancelDownload(gameId);
 };
 
 registerEvent("pauseGameSeed", pauseGameSeed);
