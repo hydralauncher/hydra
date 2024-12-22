db01980eb81136d4c3feb9fa4eaa1ed541a02494
Hachi-R <drive8hachi@gmail.com>
2024-12-22 20:20:46 -0300

    refactor: clean up code

--- src/main/events/profile/process-profile-image.ts
+++ src/main/events/profile/process-profile-image.ts
@@ -1,11 +1,14 @@
 import { registerEvent } from "../register-event";
+import { PythonRPC } from "@main/services/python-rpc";
 
 const processProfileImage = async (
   _event: Electron.IpcMainInvokeEvent,
   path: string
 ) => {
-  return path;
-  // return PythonInstance.processProfileImage(path);
+  return PythonRPC.rpc.post<{ imagePath: string; mimeType: string }>(
+    "/profile_image_processor/process_image",
+    { path },
+  );
 };
 
 registerEvent("processProfileImage", processProfileImage);
