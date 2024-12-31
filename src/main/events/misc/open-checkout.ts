3031f91fd601e9b29a80b597a188dc80329171d7
Zamitto <167933696+zamitto@users.noreply.github.com>
2024-12-31 14:06:27 -0300

    fix: issues

--- src/main/events/misc/open-checkout.ts
+++ src/main/events/misc/open-checkout.ts
@@ -1,16 +1,10 @@
 import { shell } from "electron";
 import { registerEvent } from "../register-event";
-import {
-  userAuthRepository,
-  userPreferencesRepository,
-} from "@main/repository";
+import { userAuthRepository } from "@main/repository";
 import { HydraApi } from "@main/services";
 
 const openCheckout = async (_event: Electron.IpcMainInvokeEvent) => {
-  const [userAuth, userPreferences] = await Promise.all([
-    userAuthRepository.findOne({ where: { id: 1 } }),
-    userPreferencesRepository.findOne({ where: { id: 1 } }),
-  ]);
+  const userAuth = await userAuthRepository.findOne({ where: { id: 1 } });
 
   if (!userAuth) {
     return;
@@ -22,7 +16,6 @@ const openCheckout = async (_event: Electron.IpcMainInvokeEvent) => {
 
   const params = new URLSearchParams({
     token: paymentToken,
-    lng: userPreferences?.language || "en",
   });
 
   shell.openExternal(
