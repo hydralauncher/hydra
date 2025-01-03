82b1d710c2da14002e80f416a830b9e6b5f4954b
Shisuys <gnarnd4531@gmail.com>
2025-01-03 16:58:37 -0300

    Datanodes support

---
+++ src/main/services/hosters/datanodes.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosResponse } from "axios";
+
+export class DatanodesApi {
+  private static session = axios.create({});
+
+  public static async getDownloadUrl(downloadUrl: string): Promise<string> {
+    const parsedUrl = new URL(downloadUrl);
+    const pathSegments = parsedUrl.pathname.split("/");
+
+    const fileCode = decodeURIComponent(pathSegments[1]);
+    const fileName = decodeURIComponent(pathSegments[pathSegments.length - 1]);
+
+    const headers: Record<string, string> = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Cookie: `lang=english; file_name=${fileName}; file_code=${fileCode};`,
+      Host: "datanodes.to",
+      Origin: "https://datanodes.to",
+      Referer: "https://datanodes.to/download",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    };
+
+    const payload = new URLSearchParams({
+      op: "download2",
+      id: fileCode,
+      rand: "",
+      referer: "https://datanodes.to/download",
+      method_free: "Free Download >>",
+      method_premium: "",
+      adblock_detected: "",
+    });
+
+    const config = {
+      headers,
+      maxRedirects: 0,
+      validateStatus: (status: number) => status === 302 || status < 400,
+    };
+
+    const response: AxiosResponse = await DatanodesApi.session.post(
+      "https://datanodes.to/download",
+      payload,
+      config
+    );
+
+    if (response.status === 302) {
+      return response.headers["location"];
+    }
+
+    return "";
+  }
+}
