8cf549ff05e8e0b286681ef4f336c770f36a846b
Hachi-R <drive8hachi@gmail.com>
2025-01-03 12:16:32 -0300

    refactor: enhance logging in resetGameAchievement

--- src/main/events/library/reset-game-achievements.ts
+++ src/main/events/library/reset-game-achievements.ts
@@ -2,9 +2,8 @@ import { gameAchievementRepository, gameRepository } from "@main/repository";
 import { registerEvent } from "../register-event";
 import { findAchievementFiles } from "@main/services/achievements/find-achivement-files";
 import fs from "fs";
-import { achievementsLogger, WindowManager } from "@main/services";
+import { achievementsLogger, HydraApi, WindowManager } from "@main/services";
 import { getUnlockedAchievements } from "../user/get-unlocked-achievements";
-import { HydraApi } from "@main/services/hydra-api";
 
 const resetGameAchievements = async (
   _event: Electron.IpcMainInvokeEvent,
@@ -18,12 +17,8 @@ const resetGameAchievements = async (
     const achievementFiles = findAchievementFiles(game);
 
     if (achievementFiles.length) {
-      achievementsLogger.info(
-        `deleting achievement files: ${achievementFiles
-          .map((file) => file.filePath)
-          .join(", ")}`
-      );
       for (const achievementFile of achievementFiles) {
+        achievementsLogger.log(`deleting ${achievementFile.filePath}`);
         await fs.promises.rm(achievementFile.filePath);
       }
     }
@@ -35,9 +30,9 @@ const resetGameAchievements = async (
       }
     );
 
-    await HydraApi.delete(`/profile/games/${game.remoteId}/achievements`).then(
-      (res) => console.log(res)
-    );
+    await HydraApi.delete(`/profile/games/${game.remoteId}/achievements`)
+      .catch((err) => achievementsLogger.error(err))
+      .then((res) => achievementsLogger.log(res));
 
     const gameAchievements = await getUnlockedAchievements(
       game.objectID,
