2c881a61002390a48438f6118b2ce4197c852072
Chubby Granny Chaser <hydralauncher@proton.me>
2025-01-15 17:15:57 +0000

    fix: fixing duplicate export

--- src/main/entity/index.ts
+++ src/main/entity/index.ts
@@ -1,6 +1,5 @@
 export * from "./game.entity";
 export * from "./user-preferences.entity";
 export * from "./game-shop-cache.entity";
-export * from "./game.entity";
 export * from "./game-achievements.entity";
 export * from "./download-queue.entity";
