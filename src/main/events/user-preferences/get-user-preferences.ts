f5532fa26fff0ff54108d3f9aa7a33b65aefacb8
Chubby Granny Chaser <hydralauncher@proton.me>
2025-01-22 01:57:27 +0000

    chore: encrypting real-debrid

--- src/main/events/user-preferences/get-user-preferences.ts
+++ src/main/events/user-preferences/get-user-preferences.ts
@@ -1,10 +1,21 @@
 import { registerEvent } from "../register-event";
 import { db, levelKeys } from "@main/level";
+import { Crypto } from "@main/services";
 import type { UserPreferences } from "@types";
 
 const getUserPreferences = async () =>
-  db.get<string, UserPreferences>(levelKeys.userPreferences, {
-    valueEncoding: "json",
-  });
+  db
+    .get<string, UserPreferences>(levelKeys.userPreferences, {
+      valueEncoding: "json",
+    })
+    .then((userPreferences) => {
+      if (userPreferences.realDebridApiToken) {
+        userPreferences.realDebridApiToken = Crypto.decrypt(
+          userPreferences.realDebridApiToken
+        );
+      }
+
+      return userPreferences;
+    });
 
 registerEvent("getUserPreferences", getUserPreferences);
