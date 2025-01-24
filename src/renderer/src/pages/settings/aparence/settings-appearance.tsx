7f71e0c83e59a3d08efd0545e9af612127354d82
Hachi-R <drive8hachi@gmail.com>
2025-01-24 15:29:07 -0300

    feat: use leveldb instead of localStorage

--- src/renderer/src/pages/settings/aparence/settings-appearance.tsx
+++ src/renderer/src/pages/settings/aparence/settings-appearance.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import "./settings-appearance.scss";
-import { ThemeActions } from "./index";
+import { ThemeActions, ThemeCard, ThemePlaceholder } from "./index";
+import type { Theme } from "@types";
 
 export const SettingsAppearance = () => {
+  const [themes, setThemes] = useState<Theme[]>([]);
+
+  const loadThemes = async () => {
+    const themesList = await window.electron.getAllCustomThemes();
+    setThemes(themesList);
+  };
+
+  useEffect(() => {
+    loadThemes();
+  }, []);
+
   return (
     <div className="settings-appearance">
       <p className="settings-appearance__description">Appearance</p>
 
-      <ThemeActions />
+      <ThemeActions onListUpdated={loadThemes} />
+
+      <div className="settings-appearance__themes">
+        {!themes.length ? (
+          <ThemePlaceholder onListUpdated={loadThemes} />
+        ) : (
+          themes.map((theme) => (
+            <ThemeCard
+              key={theme.id}
+              theme={theme}
+              onListUpdated={loadThemes}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
