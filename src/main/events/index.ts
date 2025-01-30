964e9bbb2b98dc07bdb50f097d63556f5a6ced0e
Hachi-R <drive8hachi@gmail.com>
2025-01-30 09:43:58 -0300

    feat: close theme editor if the theme was deleted

--- src/main/events/index.ts
+++ src/main/events/index.ts
@@ -83,6 +83,7 @@ import "./themes/open-editor-window";
 import "./themes/get-custom-theme-by-id";
 import "./themes/get-active-custom-theme";
 import "./themes/css-injector";
+import "./themes/close-editor-window";
 import { isPortableVersion } from "@main/helpers";
 
 ipcMain.handle("ping", () => "pong");
