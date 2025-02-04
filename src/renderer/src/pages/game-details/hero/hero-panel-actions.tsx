2952d448843e9ebe601945b6a8b9a30e30c92dfe
Leandro Perin <leandroperin@proton.me>
2025-02-04 19:05:11 -0300

    feat: favorites

--- src/renderer/src/pages/game-details/hero/hero-panel-actions.tsx
+++ src/renderer/src/pages/game-details/hero/hero-panel-actions.tsx
@@ -1,6 +1,8 @@
 import {
   DownloadIcon,
   GearIcon,
+  HeartFillIcon,
+  HeartIcon,
   PlayIcon,
   PlusCircleIcon,
 } from "@primer/octicons-react";
@@ -45,6 +47,30 @@ export function HeroPanelActions() {
     try {
       await window.electron.addGameToLibrary(shop, objectId!, gameTitle);
 
+      updateLibrary();
+      updateGame();
+    } finally {
+      setToggleLibraryGameDisabled(false);
+    }
+  };
+
+  const addGameToFavorites = async () => {
+    setToggleLibraryGameDisabled(true);
+
+    try {
+      await window.electron.addGameToFavorites(shop, objectId!);
+      updateLibrary();
+      updateGame();
+    } finally {
+      setToggleLibraryGameDisabled(false);
+    }
+  };
+
+  const removeGameFromFavorites = async () => {
+    setToggleLibraryGameDisabled(true);
+
+    try {
+      await window.electron.removeGameFromFavorites(shop, objectId!);
       updateLibrary();
       updateGame();
     } finally {
@@ -159,6 +185,16 @@ export function HeroPanelActions() {
       <div className="hero-panel-actions__container">
         {gameActionButton()}
         <div className="hero-panel-actions__separator" />
+        <Button
+          onClick={game.favorite ? removeGameFromFavorites : addGameToFavorites}
+          theme="outline"
+          disabled={deleting}
+          className="hero-panel-actions__action"
+        >
+          {game.favorite ? <HeartFillIcon /> : <HeartIcon />}
+        </Button>
+
+
         <Button
           onClick={() => setShowGameOptionsModal(true)}
           theme="outline"
