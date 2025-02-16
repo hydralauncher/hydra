dce1d9a6742bb80119ad618c8a2a390b080d1df8
Hachi-R <drive8hachi@gmail.com>
2025-02-15 22:18:42 -0300

    feat: add deep link theme installation support

--- src/renderer/src/pages/settings/aparence/components/theme-card.tsx
+++ src/renderer/src/pages/settings/aparence/components/theme-card.tsx
@@ -111,6 +111,7 @@ export const ThemeCard = ({ theme, onListUpdated }: ThemeCardProps) => {
 
           <div className="theme-card__actions__right">
             <Button
+              className={theme.code.startsWith("https://hydrathemes.shop/") ? "theme-card__actions__right--external" : ""}
               onClick={() => window.electron.openEditorWindow(theme.id)}
               title={t("edit_theme")}
               theme="outline"
