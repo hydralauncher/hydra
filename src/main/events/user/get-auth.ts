a32fdf33858de5ed0494e7f0b8c261a84c1f9e06
Chubby Granny Chaser <hydralauncher@proton.me>
2025-03-09 19:28:47 +0000

    style: using fs promises

--- src/main/events/user/get-auth.ts
+++ src/main/events/user/get-auth.ts
@@ -1,7 +1,7 @@
-import type { Auth } from "@types";
-import { registerEvent } from "../register-event";
-
 import { db, levelKeys } from "@main/level";
+import type { Auth } from "@types";
+
+import { registerEvent } from "../register-event";
 
 const getAuth = async (_event: Electron.IpcMainInvokeEvent) =>
   db.get<string, Auth>(levelKeys.auth, {
