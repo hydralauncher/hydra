7e67e91a4d4587bc7c98d13708345ffd9bf3d06e
Chubby Granny Chaser <hydralauncher@proton.me>
2025-04-02 11:26:15 +0100

    feat: adding recursive automatic extraction

---
+++ src/main/events/library/extract-game-download.ts
@@ -0,0 +1,46 @@
+import { registerEvent } from "../register-event";
+import { GameShop } from "@types";
+import path from "node:path";
+import { _7Zip, GameFilesManager } from "@main/services";
+import { downloadsSublevel, gamesSublevel, levelKeys } from "@main/level";
+import { FILE_EXTENSIONS_TO_EXTRACT } from "@shared";
+
+const extractGameDownload = async (
+  _event: Electron.IpcMainInvokeEvent,
+  shop: GameShop,
+  objectId: string
+): Promise<boolean> => {
+  const gameKey = levelKeys.game(shop, objectId);
+
+  const [download, game] = await Promise.all([
+    downloadsSublevel.get(gameKey),
+    gamesSublevel.get(gameKey),
+  ]);
+
+  if (!download || !game) return false;
+
+  await downloadsSublevel.put(gameKey, {
+    ...download,
+    extracting: true,
+  });
+
+  const gameFilesManager = new GameFilesManager(shop, objectId);
+
+  if (
+    FILE_EXTENSIONS_TO_EXTRACT.some((ext) => download.folderName?.endsWith(ext))
+  ) {
+    gameFilesManager.extractDownloadedFile();
+  } else {
+    gameFilesManager
+      .extractFilesInDirectory(
+        path.join(download.downloadPath, download.folderName!)
+      )
+      .then(() => {
+        gameFilesManager.setExtractionComplete(false);
+      });
+  }
+
+  return true;
+};
+
+registerEvent("extractGameDownload", extractGameDownload);
