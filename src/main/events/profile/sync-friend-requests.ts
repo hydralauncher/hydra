74d93da9b3e33be200c8d1f91a4527e9dd256f3e
Chubby Granny Chaser <hydralauncher@proton.me>
2025-05-10 17:46:17 +0100

    chore: adding protobuf ts as dev dependency

--- src/main/events/profile/sync-friend-requests.ts
+++ src/main/events/profile/sync-friend-requests.ts
@@ -1,31 +1,11 @@
 import { registerEvent } from "../register-event";
 import { HydraApi, WindowManager } from "@main/services";
-import { publishNewFriendRequestNotification } from "@main/services/notifications";
 import { UserNotLoggedInError } from "@shared";
 import type { FriendRequestSync } from "@types";
-
-interface SyncState {
-  friendRequestCount: number | null;
-  tick: number;
-}
-
-const syncState: SyncState = {
-  friendRequestCount: null,
-  tick: 0,
-};
 
 export const syncFriendRequests = async () => {
   return HydraApi.get<FriendRequestSync>(`/profile/friend-requests/sync`)
     .then((res) => {
-      if (
-        syncState.friendRequestCount != null &&
-        syncState.friendRequestCount < res.friendRequestCount
-      ) {
-        publishNewFriendRequestNotification();
-      }
-
-      syncState.friendRequestCount = res.friendRequestCount;
-
       WindowManager.mainWindow?.webContents.send(
         "on-sync-friend-requests",
         res
