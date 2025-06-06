571eeae3ed3f65f1492735fa7371a01aa013bd09
Zamitto <167933696+zamitto@users.noreply.github.com>
2025-06-06 16:52:40 -0300

    feat: add If-Modified-Since header to get-game-achievement-data

--- src/main/services/hydra-api.ts
+++ src/main/services/hydra-api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosHeaders, AxiosInstance } from "axios";
 import { WindowManager } from "./window-manager";
 import url from "url";
 import { uploadGamesBatch } from "./library-sync";
@@ -16,6 +16,7 @@ import { WSClient } from "./ws/ws-client";
 interface HydraApiOptions {
   needsAuth?: boolean;
   needsSubscription?: boolean;
+  ifModifiedSince?: Date;
 }
 
 interface HydraApiUserAuth {
@@ -337,8 +338,13 @@ export class HydraApi {
   ) {
     await this.validateOptions(options);
 
+    const headers = {
+      ...this.getAxiosConfig().headers,
+      "If-Modified-Since": options?.ifModifiedSince?.toUTCString(),
+    };
+
     return this.instance
-      .get<T>(url, { params, ...this.getAxiosConfig() })
+      .get<T>(url, { params, ...this.getAxiosConfig(), headers })
       .then((response) => response.data)
       .catch(this.handleUnauthorizedError);
   }
