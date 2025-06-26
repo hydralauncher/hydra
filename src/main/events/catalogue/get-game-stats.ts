bf8fd0dacf98d0b691f951aece4e4ebe23e68e29
Zamitto <167933696+zamitto@users.noreply.github.com>
2025-06-26 19:37:28 -0300

    feat: optimizations

--- src/main/events/catalogue/get-game-stats.ts
+++ src/main/events/catalogue/get-game-stats.ts
@@ -1,17 +1,38 @@
 import type { GameShop, GameStats } from "@types";
 import { registerEvent } from "../register-event";
 import { HydraApi } from "@main/services";
+import { gamesStatsCacheSublevel, levelKeys } from "@main/level";
+
+const LOCAL_CACHE_EXPIRATION = 1000 * 60 * 30; // 30 minutes
 
 const getGameStats = async (
   _event: Electron.IpcMainInvokeEvent,
   objectId: string,
   shop: GameShop
 ) => {
+  const cachedStats = await gamesStatsCacheSublevel.get(
+    levelKeys.game(shop, objectId)
+  );
+
+  if (
+    cachedStats &&
+    cachedStats.updatedAt + LOCAL_CACHE_EXPIRATION > Date.now()
+  ) {
+    return cachedStats;
+  }
+
   return HydraApi.get<GameStats>(
     `/games/stats`,
     { objectId, shop },
     { needsAuth: false }
-  );
+  ).then(async (data) => {
+    await gamesStatsCacheSublevel.put(levelKeys.game(shop, objectId), {
+      ...data,
+      updatedAt: Date.now(),
+    });
+
+    return data;
+  });
 };
 
 registerEvent("getGameStats", getGameStats);
