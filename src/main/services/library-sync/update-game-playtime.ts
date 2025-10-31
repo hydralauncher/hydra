c71f5947ba8dfb7856625477289bb9b2f44346f2
Zamitto <167933696+zamitto@users.noreply.github.com>
2025-10-31 10:20:11 -0300

    feat: use new ep to track game playtime

--- src/main/services/library-sync/update-game-playtime.ts
+++ src/main/services/library-sync/update-game-playtime.ts
@@ -1,7 +1,7 @@
 import type { Game } from "@types";
 import { HydraApi } from "../hydra-api";
 
-export const updateGamePlaytime = async (
+export const trackGamePlaytime = async (
   game: Game,
   deltaInMillis: number,
   lastTimePlayed: Date
@@ -10,7 +10,7 @@ export const updateGamePlaytime = async (
     return;
   }
 
-  return HydraApi.put(`/profile/games/${game.remoteId}`, {
+  return HydraApi.put(`/profile/games/${game.shop}/${game.objectId}`, {
     playTimeDeltaInSeconds: Math.trunc(deltaInMillis / 1000),
     lastTimePlayed,
   });
