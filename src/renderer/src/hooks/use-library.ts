1209a0a3a1c327c75367ba392a0202aeed22cbc9
Zamitto <167933696+zamitto@users.noreply.github.com>
2025-11-12 14:37:44 -0300

    test

--- src/renderer/src/hooks/use-library.ts
+++ src/renderer/src/hooks/use-library.ts
@@ -7,9 +7,25 @@ export function useLibrary() {
   const library = useAppSelector((state) => state.library.value);
 
   const updateLibrary = useCallback(async () => {
-    return window.electron
-      .getLibrary()
-      .then((updatedLibrary) => dispatch(setLibrary(updatedLibrary)));
+    return window.electron.getLibrary().then(async (updatedLibrary) => {
+      const libraryWithAchievements = await Promise.all(
+        updatedLibrary.map(async (game) => {
+          const unlockedAchievements =
+            await window.electron.getUnlockedAchievements(
+              game.objectId,
+              game.shop
+            );
+
+          return {
+            ...game,
+            unlockedAchievementCount:
+              game.unlockedAchievementCount || unlockedAchievements.length,
+          };
+        })
+      );
+
+      dispatch(setLibrary(libraryWithAchievements));
+    });
   }, [dispatch]);
 
   return { library, updateLibrary };
