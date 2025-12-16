3e03b8fdb010dc37c34efbcb86519e2fd944bb2a
Moyasee <terranoiz@icloud.com>
2025-12-16 15:43:43 +0200

    refactor(fullscreen-media-modal): remove unused useCallback import

--- src/renderer/src/components/fullscreen-media-modal/fullscreen-media-modal.tsx
+++ src/renderer/src/components/fullscreen-media-modal/fullscreen-media-modal.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { XIcon } from "@primer/octicons-react";
 import { useTranslation } from "react-i18next";
