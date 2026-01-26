f35d1a305dbe38c56cd5a78e782e419cc096fe07
Moyasee <terranoiz@icloud.com>
2026-01-26 10:42:43 +0200

    chore: update dependencies and refactor game launch logic

--- src/main/events/library/open-game.ts
+++ src/main/events/library/open-game.ts
@@ -1,11 +1,6 @@
 import { registerEvent } from "../register-event";
-import { shell } from "electron";
-import { spawn } from "node:child_process";
-import { parseExecutablePath } from "../helpers/parse-executable-path";
-import { gamesSublevel, levelKeys } from "@main/level";
 import { GameShop } from "@types";
-import { parseLaunchOptions } from "../helpers/parse-launch-options";
-import { WindowManager } from "@main/services";
+import { launchGame } from "@main/helpers";
 
 const openGame = async (
   _event: Electron.IpcMainInvokeEvent,
@@ -14,32 +9,7 @@ const openGame = async (
   executablePath: string,
   launchOptions?: string | null
 ) => {
-  const parsedPath = parseExecutablePath(executablePath);
-  const parsedParams = parseLaunchOptions(launchOptions);
-
-  const gameKey = levelKeys.game(shop, objectId);
-
-  const game = await gamesSublevel.get(gameKey);
-
-  if (!game) return;
-
-  await gamesSublevel.put(gameKey, {
-    ...game,
-    executablePath: parsedPath,
-    launchOptions,
-  });
-
-  // Always show the launcher window when launching a game
-  await WindowManager.createGameLauncherWindow(shop, objectId);
-
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  if (parsedParams.length === 0) {
-    shell.openPath(parsedPath);
-    return;
-  }
-
-  spawn(parsedPath, parsedParams, { shell: false, detached: true });
+  await launchGame({ shop, objectId, executablePath, launchOptions });
 };
 
 registerEvent("openGame", openGame);
