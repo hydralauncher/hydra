6fa18629953663be2b7ebd5c7d9430521c814d05
Moyasee <terranoiz@icloud.com>
2026-02-11 01:55:44 +0200

    feat: implement functionality to launch hydra in library page

--- src/types/level.types.ts
+++ src/types/level.types.ts
@@ -111,6 +111,7 @@ export interface UserPreferences {
   preferQuitInsteadOfHiding?: boolean;
   runAtStartup?: boolean;
   startMinimized?: boolean;
+  launchToLibraryPage?: boolean;
   disableNsfwAlert?: boolean;
   enableAutoInstall?: boolean;
   seedAfterDownloadComplete?: boolean;
