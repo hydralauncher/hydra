f2ec9a4fd8ff4d0deca94d74a4db765c55734e91
impieralini <impieralini@stefanini.com>
2026-03-11 21:52:32 -0300

    feat: add Croatian (hr) and Japanese (ja) translations

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,34 +1,36 @@
-import en from "./en/translation.json";
-import ptPT from "./pt-PT/translation.json";
-import ptBR from "./pt-BR/translation.json";
-import es from "./es/translation.json";
-import nl from "./nl/translation.json";
-import fr from "./fr/translation.json";
-import hu from "./hu/translation.json";
-import it from "./it/translation.json";
-import de from "./de/translation.json";
-import pl from "./pl/translation.json";
-import ru from "./ru/translation.json";
-import tr from "./tr/translation.json";
-import be from "./be/translation.json";
-import uk from "./uk/translation.json";
-import zh from "./zh/translation.json";
-import id from "./id/translation.json";
-import ko from "./ko/translation.json";
-import da from "./da/translation.json";
 import ar from "./ar/translation.json";
-import fa from "./fa/translation.json";
-import ro from "./ro/translation.json";
-import ca from "./ca/translation.json";
-import kk from "./kk/translation.json";
-import cs from "./cs/translation.json";
-import nb from "./nb/translation.json";
-import et from "./et/translation.json";
+import be from "./be/translation.json";
 import bg from "./bg/translation.json";
-import uz from "./uz/translation.json";
+import ca from "./ca/translation.json";
+import cs from "./cs/translation.json";
+import da from "./da/translation.json";
+import de from "./de/translation.json";
+import en from "./en/translation.json";
+import es from "./es/translation.json";
+import et from "./et/translation.json";
+import fa from "./fa/translation.json";
 import fi from "./fi/translation.json";
-import sv from "./sv/translation.json";
+import fr from "./fr/translation.json";
+import hr from "./hr/translation.json";
+import hu from "./hu/translation.json";
+import id from "./id/translation.json";
+import it from "./it/translation.json";
+import ja from "./ja/translation.json";
+import kk from "./kk/translation.json";
+import ko from "./ko/translation.json";
 import lv from "./lv/translation.json";
+import nb from "./nb/translation.json";
+import nl from "./nl/translation.json";
+import pl from "./pl/translation.json";
+import ptBR from "./pt-BR/translation.json";
+import ptPT from "./pt-PT/translation.json";
+import ro from "./ro/translation.json";
+import ru from "./ru/translation.json";
+import sv from "./sv/translation.json";
+import tr from "./tr/translation.json";
+import uk from "./uk/translation.json";
+import uz from "./uz/translation.json";
+import zh from "./zh/translation.json";
 
 export default {
   "pt-BR": ptBR,
@@ -62,4 +64,6 @@ export default {
   uz,
   sv,
   lv,
+  hr,
+  ja,
 };
