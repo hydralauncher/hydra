8e70e2d54af09054d5d32b9d76edae2dfb0f5388
japa2k <teruyuki709@gmail.com>
2026-04-16 18:52:42 -0300

    feat(big-picture): initialize Big Picture module with HTML, Vite config, and main app structure

---
+++ src/big-picture/src/app.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <div>
+      <button
+        type="button"
+        style={{ color: "white" }}
+        onClick={() => window.close()}
+      >
+        close
+      </button>
+      <h1 style={{ color: "white" }}>Big Picture</h1>
+    </div>
+  );
+}
---
+++ src/big-picture/vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: resolve(__dirname),
+  build: {
+    outDir: resolve(__dirname, "../../dist/big-picture"),
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@shared": resolve(__dirname, "../../src/shared"),
+      "@locales": resolve(__dirname, "../../src/locales"),
+    },
+  },
+  plugins: [react()],
+});
