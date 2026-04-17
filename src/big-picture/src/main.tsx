44adff29a5714e0d96a3f92b65871753e848d574
japa2k <teruyuki709@gmail.com>
2026-04-17 15:37:38 -0300

    refactor(big-picture): change App export to default and update import in main

--- src/big-picture/src/main.tsx
+++ src/big-picture/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
