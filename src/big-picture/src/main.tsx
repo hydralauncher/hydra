669e7f7bcd49b3dabcdb00a11df8a15325fe6426
japa2k <teruyuki709@gmail.com>
2026-04-17 22:39:59 -0300

    feat(big-picture): implement routing and add Catalogue, Downloads, and Settings pages

--- src/big-picture/src/main.tsx
+++ src/big-picture/src/main.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./app";
+import Catalogue from "./pages/catalogue/catalogue";
+import Downloads from "./pages/downloads/downloads";
+import Settings from "./pages/settings/settings";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="catalogue" element={<Catalogue />} />
+          <Route path="downloads" element={<Downloads />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
