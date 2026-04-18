2e95157ef8e87902e01b212ee54d708ae2f6ec54
japa2k <teruyuki709@gmail.com>
2026-04-18 03:44:22 -0300

    feat(big-picture): add Library page functionality with sorting and searching capabilities

---
+++ src/big-picture/src/hooks/use-library.hook.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from "react";
+import { IS_DESKTOP } from "../constants";
+import type { LibraryGame } from "@types";
+
+export function useLibrary() {
+  const [library, setLibrary] = useState<LibraryGame[]>([]);
+
+  const updateLibrary = useCallback(async () => {
+    if (!IS_DESKTOP) return;
+    const updatedLibrary = await window.electron.getLibrary();
+    setLibrary(updatedLibrary);
+  }, []);
+
+  return { library, updateLibrary };
+}
