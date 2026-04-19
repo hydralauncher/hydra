42b45a011395ff776009a27332c00e6e969c217b
japa2k <teruyuki709@gmail.com>
2026-04-18 21:56:33 -0300

    feat(big-picture): add GameCard component with styling and structure

---
+++ src/big-picture/src/components/game-card/game-card.tsx
@@ -0,0 +1,23 @@
+import "./game-card.scss";
+
+interface GameCardProps {
+  coverImageUrl?: string | null;
+  gameTitle: string;
+}
+
+function GameCard({ coverImageUrl, gameTitle }: GameCardProps) {
+  return (
+    <div className="big-picture__game-card">
+      <div className="big-picture__game-card__cover">
+        {coverImageUrl ? (
+          <img src={coverImageUrl} alt={gameTitle} draggable={false} />
+        ) : (
+          <div className="big-picture__game-card__cover--placeholder" />
+        )}
+      </div>
+      <span className="big-picture__game-card__title">{gameTitle}</span>
+    </div>
+  );
+}
+
+export { GameCard };
