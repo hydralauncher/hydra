a919421fb85e8c5c9a52aa9e642c01f29a12f17f
Hachi-R <drive8hachi@gmail.com>
2026-04-20 23:04:06 -0300

    feat(big-picture): export services for gamepad and navigation functionalities

---
+++ src/big-picture/src/services/index.ts
@@ -0,0 +1,5 @@
+export * from "./gamepad.service";
+export * from "./navigation.service";
+export * from "./navigation-focus-bridge.service";
+export * from "./navigation-item-actions.service";
+export * from "./navigation-screen-actions.service";
