2e699871e35351dd69f63d1ad26b7161d33dbca0
japa2k <teruyuki709@gmail.com>
2026-04-22 18:16:21 -0300

    feat(big-picture): enhance sidebar with library and profile components for improved navigation and user experience

--- src/big-picture/src/layout/sidebar/index.tsx
+++ src/big-picture/src/layout/sidebar/index.tsx
@@ -1,14 +1,26 @@
 import {
   BookOpenIcon,
   DownloadSimpleIcon,
+  FunnelSimpleIcon,
   GearIcon,
   HouseIcon,
+  MagnifyingGlassIcon,
   SquaresFourIcon,
 } from "@phosphor-icons/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { IS_DESKTOP } from "../../constants";
-
+import {
+  Button,
+  Divider,
+  Input,
+  RouteAnchor,
+  ScrollArea,
+  UserProfile,
+} from "../../components";
 import "./styles.scss";
+import { useLibrary, useSearch, useUserDetails } from "../../hooks";
+import { useMemo } from "react";
+import { toSlug } from "../../helpers";
 
 function SidebarRouter() {
   const basePath = IS_DESKTOP ? "/big-picture" : "";
@@ -77,12 +89,90 @@ function SidebarRouter() {
   );
 }
 
+function SidebarLibrary() {
+  const { library } = useLibrary();
+
+  const sortedLibrary = useMemo(() => {
+    return library.sort(
+      (a, b) =>
+        (b.playTimeInMilliseconds ?? 0) - (a.playTimeInMilliseconds ?? 0)
+    );
+  }, [library]);
+
+  const { filteredItems, search, setSearch } = useSearch(sortedLibrary, [
+    "title",
+  ]);
+
+  return (
+    <div className="library-container">
+      <div className="library-container__header">
+        <Input
+          placeholder="Search"
+          iconLeft={<MagnifyingGlassIcon size={24} />}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          spellCheck={false}
+          autoComplete="off"
+        />
+
+        <Button variant="rounded" size="icon">
+          <FunnelSimpleIcon
+            size={24}
+            className="library-container__header__icon"
+          />
+        </Button>
+      </div>
+
+      <ScrollArea>
+        <ul className="library-list">
+          {filteredItems.map((game) => (
+            <li key={game.id} className="library-list__item">
+              <RouteAnchor
+                key={game.id}
+                label={game.title}
+                href={`/game/${game.objectId}/${toSlug(game.title)}`}
+                icon={game.iconUrl}
+                isFavorite={game.favorite}
+              />
+            </li>
+          ))}
+        </ul>
+      </ScrollArea>
+    </div>
+  );
+}
+
+function SidebarProfile() {
+  const { userDetails } = useUserDetails();
+
+  return (
+    <div className="bp-sidebar-profile">
+      <UserProfile
+        name={userDetails?.displayName ?? ""}
+        image={userDetails?.profileImageUrl}
+        friendCode={userDetails?.id ?? ""}
+      />
+    </div>
+  );
+}
+
 function SidebarContainer({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const handleMouseLeave = () => {
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    }
+  };
+
   return (
     <>
-      <div className="big-picture__sidebar-container">{children}</div>
+      <div
+        className="big-picture__sidebar-container"
+        onMouseLeave={handleMouseLeave}
+      >
+        {children}
+      </div>
       <div className="big-picture__sidebar-spacer" />
       <div className="big-picture__sidebar-drawer-overlay" />
     </>
@@ -93,6 +183,9 @@ function Sidebar() {
   return (
     <SidebarContainer>
       <SidebarRouter />
+      <Divider />
+      <SidebarLibrary />
+      <SidebarProfile />
     </SidebarContainer>
   );
 }
