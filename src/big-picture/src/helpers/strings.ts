a7020a29e75e6cf4fd1963576109c034d376fd46
japa2k <teruyuki709@gmail.com>
2026-04-22 17:28:11 -0300

    feat(big-picture): add string utility function to convert names to slugs

---
+++ src/big-picture/src/helpers/strings.ts
@@ -0,0 +1,10 @@
+export const toSlug = (name: string) => {
+  return name
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9\s-]/g, "")
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
