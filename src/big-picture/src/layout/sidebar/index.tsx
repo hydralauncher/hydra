0c48b1a9ca116ec1e75e693f73e30d854341c153
japa2k <teruyuki709@gmail.com>
2026-04-23 00:28:03 -0300

    refactor(big-picture): enhance sidebar layout and accessibility by replacing divs with RouteAnchor and integrating VerticalFocusGroup

--- src/big-picture/src/layout/sidebar/index.tsx
+++ src/big-picture/src/layout/sidebar/index.tsx
@@ -7,25 +7,24 @@ import {
   MagnifyingGlassIcon,
   SquaresFourIcon,
 } from "@phosphor-icons/react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { IS_DESKTOP } from "../../constants";
+import { useMemo } from "react";
 import {
   Button,
   Divider,
+  HorizontalFocusGroup,
   Input,
   RouteAnchor,
   ScrollArea,
   UserProfile,
+  VerticalFocusGroup,
 } from "../../components";
-import "./styles.scss";
-import { useLibrary, useSearch, useUserDetails } from "../../hooks";
-import { useMemo } from "react";
+import { IS_DESKTOP } from "../../constants";
 import { toSlug } from "../../helpers";
+import { useLibrary, useSearch, useUserDetails } from "../../hooks";
+import "./styles.scss";
 
 function SidebarRouter() {
   const basePath = IS_DESKTOP ? "/big-picture" : "";
-
-  const navigate = useNavigate();
 
   const routes = [
     {
@@ -55,36 +54,16 @@ function SidebarRouter() {
     },
   ];
 
-  const { pathname } = useLocation();
-
-  const handleSidebarItemClick = (path: string) => {
-    if (path !== pathname) {
-      navigate(path);
-    }
-  };
-
   return (
-    <div className="big-picture__router-container">
-      {routes.map((route) => {
-        const active = pathname === route.path;
-        return (
-          <div
-            key={route.path}
-            className={`state-wrapper${active ? " state-wrapper--active" : ""}`}
-          >
-            <button
-              type="button"
-              onClick={() => handleSidebarItemClick(route.path)}
-              className={`route-anchor route-anchor--extra-padding${active ? " route-anchor--active" : ""}`}
-            >
-              <div className="route-anchor__icon route-anchor__icon--small-size">
-                <route.icon size={24} />
-              </div>
-              <div className="route-anchor__label">{route.label}</div>
-            </button>
-          </div>
-        );
-      })}
+    <div className="sidebar-router-container">
+      {routes.map((route) => (
+        <RouteAnchor
+          key={route.label}
+          label={route.label}
+          href={route.path}
+          icon={<route.icon size={24} />}
+        />
+      ))}
     </div>
   );
 }
@@ -106,21 +85,23 @@ function SidebarLibrary() {
   return (
     <div className="library-container">
       <div className="library-container__header">
-        <Input
-          placeholder="Search"
-          iconLeft={<MagnifyingGlassIcon size={24} />}
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          spellCheck={false}
-          autoComplete="off"
-        />
-
-        <Button variant="rounded" size="icon">
-          <FunnelSimpleIcon
-            size={24}
-            className="library-container__header__icon"
+        <HorizontalFocusGroup regionId="sidebar-library-search">
+          <Input
+            placeholder="Search"
+            iconLeft={<MagnifyingGlassIcon size={24} />}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            spellCheck={false}
+            autoComplete="off"
           />
-        </Button>
+
+          <Button variant="rounded" size="icon">
+            <FunnelSimpleIcon
+              size={24}
+              className="library-container__header__icon"
+            />
+          </Button>
+        </HorizontalFocusGroup>
       </div>
 
       <ScrollArea>
@@ -146,7 +127,7 @@ function SidebarProfile() {
   const { userDetails } = useUserDetails();
 
   return (
-    <div className="bp-sidebar-profile">
+    <div className="sidebar-profile">
       <UserProfile
         name={userDetails?.displayName ?? ""}
         image={userDetails?.profileImageUrl}
@@ -167,26 +148,25 @@ function SidebarContainer({
 
   return (
     <>
-      <div
-        className="big-picture__sidebar-container"
-        onMouseLeave={handleMouseLeave}
-      >
+      <div className="sidebar-container" onMouseLeave={handleMouseLeave}>
         {children}
       </div>
-      <div className="big-picture__sidebar-spacer" />
-      <div className="big-picture__sidebar-drawer-overlay" />
+      <div className="sidebar-spacer" />
+      <div className="sidebar-drawer-overlay" />
     </>
   );
 }
 
 function Sidebar() {
   return (
-    <SidebarContainer>
-      <SidebarRouter />
-      <Divider />
-      <SidebarLibrary />
-      <SidebarProfile />
-    </SidebarContainer>
+    <VerticalFocusGroup regionId="sidebar">
+      <SidebarContainer>
+        <SidebarRouter />
+        <Divider />
+        <SidebarLibrary />
+        <SidebarProfile />
+      </SidebarContainer>
+    </VerticalFocusGroup>
   );
 }
 
