910096e476b6b934b50a5ce355caf0ac1961f80c
japa2k <teruyuki709@gmail.com>
2026-04-23 00:26:00 -0300

    feat(big-picture): add PostCSS plugin for scoping CSS styles to the big picture component

--- src/big-picture/vite.config.ts
+++ src/big-picture/vite.config.ts
@@ -1,12 +1,18 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { scopeBigPictureCss } from "./vite-scope-big-picture-css";
 
 export default defineConfig({
   root: resolve(__dirname),
   build: {
     outDir: resolve(__dirname, "../../dist/big-picture"),
     emptyOutDir: true,
+  },
+  css: {
+    postcss: {
+      plugins: [scopeBigPictureCss()],
+    },
   },
   resolve: {
     alias: {
