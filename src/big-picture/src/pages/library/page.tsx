2972abdca2223b28ba521290288e1ff51edd8179
Hachi-R <drive8hachi@gmail.com>
2026-04-24 01:13:15 -0300

    refactor(big-picture): simplify LibraryPage layout by removing unused elements and integrating LibraryHero for last played games display

--- src/big-picture/src/pages/library/page.tsx
+++ src/big-picture/src/pages/library/page.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useMemo } from "react";
 import { IS_DESKTOP } from "../../constants";
 import { useLibrary } from "../../hooks";
+import { LibraryHero } from "../../components";
+
 import "./page.scss";
-import { FocusItem, GameCard, GridFocusGroup } from "../../components";
+
+const LAST_PLAYED_GAMES_COUNT = 3;
 
 export default function LibraryPage() {
   const { library, updateLibrary } = useLibrary();
@@ -33,7 +36,19 @@ export default function LibraryPage() {
     return sortedLibrary;
   }, [sortedLibrary]);
 
-  if (library.length === 0) {
+  const lastPlayedGames = useMemo(() => {
+    return [...filteredLibrary]
+      .filter((game) => game.lastTimePlayed != null)
+      .sort((a, b) => {
+        const aLastPlayed = new Date(a.lastTimePlayed as Date).getTime();
+        const bLastPlayed = new Date(b.lastTimePlayed as Date).getTime();
+
+        return bLastPlayed - aLastPlayed;
+      })
+      .slice(0, LAST_PLAYED_GAMES_COUNT);
+  }, [filteredLibrary]);
+
+  if (library.length === 0 && lastPlayedGames.length === 0) {
     return (
       <div className="library-page__empty">
         <p>No games in library</p>
@@ -43,20 +58,7 @@ export default function LibraryPage() {
 
   return (
     <section className="library-page">
-      <div className="library-page__toolbar">
-        <h1 className="library-page__toolbar__title">Library</h1>
-      </div>
-
-      <GridFocusGroup className="library-page__grid">
-        {filteredLibrary.map((game) => (
-          <FocusItem id={game.objectId} key={game.objectId}>
-            <GameCard
-              coverImageUrl={game.coverImageUrl}
-              gameTitle={game.title}
-            />
-          </FocusItem>
-        ))}
-      </GridFocusGroup>
+      <LibraryHero lastPlayedGames={lastPlayedGames} />
     </section>
   );
 }
