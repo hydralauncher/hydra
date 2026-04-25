83408752d2fdb408c38f249945b7a654e5dfafcb
japa2k <teruyuki709@gmail.com>
2026-04-25 19:56:34 -0300

    feat(achievements): add focus navigation overrides and enhance accessibility for achievements component

--- src/big-picture/src/components/pages/game/achievements/index.tsx
+++ src/big-picture/src/components/pages/game/achievements/index.tsx
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom";
 import { Box, FocusItem, Typography } from "../../../common";
 import cn from "classnames";
 import { EyeIcon } from "@phosphor-icons/react/dist/ssr";
+import {
+  GAME_ACHIEVEMENTS_TITLE_ID,
+  GAME_ACHIEVEMENTS_VIEW_ALL_ID,
+  GAME_REQUIREMENTS_TO_PLAY_MINIMUM_BUTTON_ID,
+  GAME_SCREENSHOT_CAROUSEL_NEXT_BUTTON_ID,
+  GAME_SCREENSHOT_CAROUSEL_PREV_BUTTON_ID,
+} from "../navigation";
+import { FocusOverrides } from "src/big-picture/src/services/navigation.service";
 
 export interface AchievementsBoxProps {
   achievements: UserAchievement[];
@@ -11,9 +19,45 @@ export interface AchievementsBoxProps {
 export function AchievementsBox({
   achievements,
 }: Readonly<AchievementsBoxProps>) {
+  const achievementsNavigationOverrides: FocusOverrides = {
+    down: {
+      type: "item",
+      itemId: GAME_ACHIEVEMENTS_VIEW_ALL_ID,
+    },
+    right: {
+      type: "block",
+    },
+    left: {
+      type: "item",
+      itemId: GAME_SCREENSHOT_CAROUSEL_NEXT_BUTTON_ID,
+    },
+  };
+
+  const viewAllNavigationOverrides: FocusOverrides = {
+    up: {
+      type: "item",
+      itemId: GAME_ACHIEVEMENTS_TITLE_ID,
+    },
+    left: {
+      type: "item",
+      itemId: GAME_SCREENSHOT_CAROUSEL_PREV_BUTTON_ID,
+    },
+    right: {
+      type: "block",
+    },
+    down: {
+      type: "item",
+      itemId: GAME_REQUIREMENTS_TO_PLAY_MINIMUM_BUTTON_ID,
+    },
+  };
+
   return (
     <div className="game-page__box-group">
-      <FocusItem asChild>
+      <FocusItem
+        id={GAME_ACHIEVEMENTS_TITLE_ID}
+        navigationOverrides={achievementsNavigationOverrides}
+        asChild
+      >
         <div className="game-page__achievements-title">
           <Typography>Achievements</Typography>
 
@@ -54,7 +98,10 @@ export function AchievementsBox({
 
         <div className="game-page__achievement-box-content">
           <div className="game-page__achievement-box-link">
-            <FocusItem>
+            <FocusItem
+              id={GAME_ACHIEVEMENTS_VIEW_ALL_ID}
+              navigationOverrides={viewAllNavigationOverrides}
+            >
               <Link to="/big-picture/library">
                 <Typography>View All Achievements</Typography>
               </Link>
