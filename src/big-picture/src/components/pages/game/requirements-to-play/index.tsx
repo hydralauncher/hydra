c968402f588422db07eb5f0b408531e9d8fb3df2
japa2k <teruyuki709@gmail.com>
2026-04-25 19:56:50 -0300

    feat(requirements): add focus navigation overrides for minimum and recommended buttons to enhance accessibility

--- src/big-picture/src/components/pages/game/requirements-to-play/index.tsx
+++ src/big-picture/src/components/pages/game/requirements-to-play/index.tsx
@@ -1,7 +1,14 @@
 import { ShopDetails } from "@types";
 import { useMemo, useState } from "react";
+import { FocusOverrides } from "src/big-picture/src/services/navigation.service";
 import { normalizeRequirementsHtml } from "../../../../helpers";
 import { Box, Button, HorizontalFocusGroup, Typography } from "../../../common";
+import {
+  GAME_ACHIEVEMENTS_VIEW_ALL_ID,
+  GAME_REQUIREMENTS_TO_PLAY_BUTTONS_REGION_ID,
+  GAME_REQUIREMENTS_TO_PLAY_MINIMUM_BUTTON_ID,
+  GAME_REQUIREMENTS_TO_PLAY_RECOMMENDED_BUTTON_ID,
+} from "../navigation";
 
 export interface RequirementsToPlayProps {
   shopDetails: ShopDetails;
@@ -23,6 +30,34 @@ export function RequirementsToPlay({
     return normalizeRequirementsHtml(raw);
   }, [activeRequirement, shopDetails.pc_requirements]);
 
+  const minimumButtonNavigationOverrides: FocusOverrides = {
+    up: {
+      type: "item",
+      itemId: GAME_ACHIEVEMENTS_VIEW_ALL_ID,
+    },
+    left: {
+      type: "block",
+    },
+    right: {
+      type: "item",
+      itemId: GAME_REQUIREMENTS_TO_PLAY_RECOMMENDED_BUTTON_ID,
+    },
+  };
+
+  const recommendedButtonNavigationOverrides: FocusOverrides = {
+    up: {
+      type: "item",
+      itemId: GAME_ACHIEVEMENTS_VIEW_ALL_ID,
+    },
+    left: {
+      type: "item",
+      itemId: GAME_REQUIREMENTS_TO_PLAY_MINIMUM_BUTTON_ID,
+    },
+    right: {
+      type: "block",
+    },
+  };
+
   return (
     <div className="game-page__box-group">
       <div className="game-page__requirements-to-play-header">
@@ -30,9 +65,13 @@ export function RequirementsToPlay({
           <Typography>System Requirements</Typography>
         </div>
 
-        <HorizontalFocusGroup regionId="requirements-to-play-buttons">
+        <HorizontalFocusGroup
+          regionId={GAME_REQUIREMENTS_TO_PLAY_BUTTONS_REGION_ID}
+        >
           <div className="game-page__requirements-to-play-buttons">
             <Button
+              focusId={GAME_REQUIREMENTS_TO_PLAY_MINIMUM_BUTTON_ID}
+              focusNavigationOverrides={minimumButtonNavigationOverrides}
               onClick={() => setActiveRequirement("minimum")}
               variant={activeRequirement === "minimum" ? "primary" : "rounded"}
             >
@@ -40,6 +79,8 @@ export function RequirementsToPlay({
             </Button>
 
             <Button
+              focusId={GAME_REQUIREMENTS_TO_PLAY_RECOMMENDED_BUTTON_ID}
+              focusNavigationOverrides={recommendedButtonNavigationOverrides}
               onClick={() => setActiveRequirement("recommended")}
               variant={
                 activeRequirement === "recommended" ? "primary" : "rounded"
