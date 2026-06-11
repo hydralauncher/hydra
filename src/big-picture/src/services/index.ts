0e29f76766cbd70d3389083ba28606b84ac37440
Hachi-R <drive8hachi@gmail.com>
2026-06-11 17:20:30 -0300

    feat: implement navigation audio service to play sound effects on focus movement and item selection

--- src/big-picture/src/services/index.ts
+++ src/big-picture/src/services/index.ts
@@ -1,5 +1,6 @@
 export * from "./gamepad.service";
 export * from "./navigation.service";
+export * from "./navigation-audio.service";
 export * from "./navigation-focus-bridge.service";
 export * from "./navigation-item-actions.service";
 export * from "./navigation-screen-actions.service";
