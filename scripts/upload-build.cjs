1e75dc8e874103f8e8bdd2d185ab7f30c208b88f
VictorVow <12621257+VictorVow@users.noreply.github.com>
2026-06-24 21:38:19 +0100

    feat(release): build and publish Flatpak in the release pipeline

--- scripts/upload-build.cjs
+++ scripts/upload-build.cjs
@@ -23,7 +23,7 @@ const s3 = new S3Client({
 
 const dist = path.resolve(__dirname, "..", "dist");
 
-const extensionsToUpload = [".deb", ".exe", ".AppImage"];
+const extensionsToUpload = [".deb", ".exe", ".AppImage", ".flatpak"];
 
 fs.readdir(dist, async (err, files) => {
   if (err) throw err;
