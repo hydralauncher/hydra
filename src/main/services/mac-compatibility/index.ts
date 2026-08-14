1179fc69e32ac06751978d1f7fe42fa279fd80c2
apxllo123 <danwantstocode@outlook.com>
2026-08-13 23:06:21 -0500

    Add index.ts for mac compatibility exports

---
+++ src/main/services/mac-compatibility/index.ts
@@ -0,0 +1,33 @@
+export { MacCompatibilityManager } from "./MacCompatibilityManager";
+export { MacCompatibilityRegistry } from "./MacCompatibilityRegistry";
+export { MacGameManager } from "./MacGameManager";
+export { MacSystemDetector } from "./MacSystemDetector";
+export { MacWineDetector } from "./MacWineDetector";
+
+export {
+  MacWineEnvironmentHealthChecker,
+  MacWineEnvironmentInitializer,
+  MacWineEnvironmentManager,
+  MacWineEnvironmentRegistry,
+  MacWineEnvironmentRepairer,
+} from "./environment";
+
+export type {
+  MacArchitecture,
+  MacCompatibilityAction,
+  MacCompatibilityCheckResult,
+  MacCompatibilityGameKey,
+  MacCompatibilityIssue,
+  MacCompatibilityLevel,
+  MacCompatibilityOperationProgress,
+  MacCompatibilityOperationResult,
+  MacCompatibilityPlatform,
+  MacCompatibilityRecommendation,
+  MacCompatibilityRegistryEntry,
+  MacCompatibilityStatus,
+  MacGameCompatibility,
+  MacSystemInfo,
+  MacWineEnvironment,
+  MacWineType,
+  MacWineVersion,
+} from "./MacCompatibilityTypes";
