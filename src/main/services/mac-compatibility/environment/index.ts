f1f557dd0c5aed30e9e324d2fd45a474b28ee312
apxllo123 <danwantstocode@outlook.com>
2026-08-13 23:04:21 -0500

    Create index.ts for Mac compatibility exports

---
+++ src/main/services/mac-compatibility/environment/index.ts
@@ -0,0 +1,5 @@
+export { MacWineEnvironmentManager } from "./MacWineEnvironmentManager";
+export { MacWineEnvironmentRegistry } from "./MacWineEnvironmentRegistry";
+export { MacWineEnvironmentInitializer } from "./MacWineEnvironmentInitializer";
+export { MacWineEnvironmentHealthChecker } from "./MacWineEnvironmentHealthChecker";
+export { MacWineEnvironmentRepairer } from "./MacWineEnvironmentRepairer";
