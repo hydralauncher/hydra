8496b9a77c28187288c549519511d428b3e2841c
apxllo <danwantstocode@outlook.com>
2026-08-14 09:09:39 -0500

    Fix type declaration for translations object

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -34,7 +34,9 @@ import vi from "./vi/translation.json";
 import ja from "./ja/translation.json";
 import sl from "./sl/translation.json";
 
-const translations = {
+type Translation = Record<string, unknown>;
+
+const translations: Record<string, Translation> = {
   "pt-BR": ptBR,
   "pt-PT": ptPT,
   en,
