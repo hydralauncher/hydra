b7ae1bbd1cefcdb0d3b5d330dabce907733b82cb
apxllo <danwantstocode@outlook.com>
2026-08-14 18:46:03 -0500

    Update index.ts

--- src/main/services/mac-compatibility/environment/index.ts
+++ src/main/services/mac-compatibility/environment/index.ts
@@ -3,3 +3,12 @@ export { MacWineEnvironmentRegistry } from "./MacWineEnvironmentRegistry";
 export { MacWineEnvironmentInitializer } from "./MacWineEnvironmentInitializer";
 export { MacWineEnvironmentHealthChecker } from "./MacWineEnvironmentHealthChecker";
 export { MacWineEnvironmentRepairer } from "./MacWineEnvironmentRepairer";
+export {
+  DEFAULT_MAC_ENVIRONMENTS_PATH,
+  DEFAULT_MAC_ENVIRONMENTS_REGISTRY_PATH,
+  assertManagedPrefixPath,
+  assertPathInsidePrefix,
+  createEnvironmentId,
+  resolveManagedPrefixPath,
+  sanitizeEnvironmentIdPart,
+} from "./MacWineEnvironmentPaths";
